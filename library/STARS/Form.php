fe92b76851b277fce03165b9c18ec9fe2df7a133
julia <julia@localhost>
2008-06-10 22:51:51 +0000

    trying to fix issue with paths

--- library/STARS/Form.php
+++ library/STARS/Form.php
@@ -6,8 +6,8 @@ class STARS_Form extends Zend_Form
     
     public function __construct($config = null)
     {
-        $this->addPrefixPath('STARS', 'STARS/');
-        $this->addElementPrefixPath('STARS', 'STARS/');
+        $this->addPrefixPath('STARS', 'STARS/TRUNK');
+        $this->addElementPrefixPath('STARS', 'STARS/TRUNK');
         
         // This may seem like weird placement, but you need to add the 
         // prefixes before the elements and the decorators after.
