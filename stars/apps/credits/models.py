4a574e6e4636e91a5e1d8a8504283eaa3cca0ac1
joseph <joseph@localhost>
2009-10-20 16:26:07 +0000

    Fixes #286  SCHEMA change:
    BEFORE applying this patch and schema change, be sure to migrate is_required field data!!  (see previous commits.)
    alter table credits_documentationfield delete is_required

--- stars/apps/credits/models.py
+++ stars/apps/credits/models.py
@@ -514,7 +514,6 @@ class DocumentationField(models.Model):
     inline_help_text = models.TextField(null=True, blank=True)
     tooltip_help_text = models.TextField(null=True, blank=True)
     ordinal = models.SmallIntegerField(default=-1)
-    is_required = models.BooleanField(default=True)
     required = models.CharField(max_length=8, choices=REQUIRED_TYPES, default='req', help_text='If a field is conditionally required it is important to note that in the help-text and to define a custom validation rule.')
     is_confidential = models.BooleanField()
     identifier = models.CharField(max_length=2) # editable=False) # Field identifier for the Formula editor - auto-generated.
