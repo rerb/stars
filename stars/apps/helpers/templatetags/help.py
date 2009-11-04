f3c8005865bce87d7409c417df76f5ea92dd28de
ben <ben@localhost>
2009-11-04 15:22:50 +0000

    Fixed the javascript encoding for tooltips, per #284.

--- stars/apps/helpers/templatetags/help.py
+++ stars/apps/helpers/templatetags/help.py
@@ -2,6 +2,8 @@ from django import template
 from django.utils.html import strip_spaces_between_tags, escape
 from django.utils.safestring import mark_safe 
 
+import re
+
 from stars.apps.helpers.models import HelpContext
 
 register = template.Library()
@@ -25,10 +27,8 @@ def show_help_text(help_text, as_tooltip=True):
 
 def _clean(text, as_tooltip):
     """ Helper to prepare the help text """
-    return mark_safe(
-             strip_spaces_between_tags( 
-               text.strip().
-               replace('"', '\&quot;' if as_tooltip else '&quot;').
-               replace("'", '\&#39;'  if as_tooltip else '&#39;') 
-             )
-           )
+    js_encoded = escape(strip_spaces_between_tags(text.strip()))
+    if as_tooltip:
+        js_encoded = re.sub(r'\r\n|\r|\n', '', js_encoded)
+        js_encoded = js_encoded.replace('&quot;', '\&quot;').replace("&amp;", '\&amp;').replace("&#39;", '\&#39;')
+    return mark_safe(js_encoded)
