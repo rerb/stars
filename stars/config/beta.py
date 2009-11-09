d5384d3968c3461154322b6b4041afed6f29b265
ben <ben@localhost>
2009-11-09 05:29:55 +0000

    Added beta config file.

---
+++ stars/config/beta.py
@@ -0,0 +1,24 @@
+"""
+    This file configures the development environment on the aashe server.
+    Apache bypasses settings.py and calls this file explicitly for setup.
+"""
+
+from settings import *
+
+DEBUG = True
+
+DATABASE_ENGINE = 'mysql'
+DATABASE_NAME = 'stars_beta'
+DATABASE_USER = 'starsapp'
+DATABASE_PASSWORD = 'J3z4#$szFET--6'
+DATABASE_HOST = 'localhost'
+
+MEDIA_ROOT = '/var/www/stars.beta.aashe.org/media'
+
+SSO_SERVER_URI = WWW_SSO_SERVER_URI
+STARS_DOMAIN = DEV_STARS_DOMAIN
+SSO_API_KEY = DEV_SSO_API_KEY
+
+XMLRPC_VERBOSE = False
+
+CYBERSOURCE_URL = CYBERSOURCE_TEST_URL
