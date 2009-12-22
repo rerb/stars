a19e06340b001a50204e7419bd890ae660caa57d
joseph <joseph@localhost>
2009-12-22 20:22:56 +0000

    Another round of minor UI tweaks and improvements for Scorecard Reports - #363

--- stars/apps/institutions/templatetags/report_tags.py
+++ stars/apps/institutions/templatetags/report_tags.py
@@ -17,9 +17,12 @@ def show_report_crumbs(object):
     return {'object_set': object_set}
 
 
-@register.inclusion_tag('institutions/tags/field_value.html')
-def show_field_value(field):
-    """ Displays a submission field value in the Reports Tool """
+@register.inclusion_tag('institutions/tags/reporting_field.html')
+def show_reporting_field(field, hide_empty_field=False):
+    """ 
+        Displays a submission reporing field in the Reports Tool 
+        Optionally, it will hide fields that do not have any value submitted 
+    """
     format_method = { 
             'text': _get_text_context,
             'long_text': _get_text_context,
@@ -32,27 +35,32 @@ def show_field_value(field):
             'upload': _get_upload_context,
     }
     context = format_method.get(field.documentation_field.type, _get_text_context)(field)
-            
+    
+    context.update({'show_field': ((not hide_empty_field) or (context['field_value'] is not None)),
+                    'field': field
+                   })
+    
     return context
 
 def _get_text_context(field):
     text = field.get_value()
     units = field.documentation_field.units
-    return {'field_value' : text, 'units': units if units else ''}
+    return {'field_value' : text if text else None, 'units': units if (text and units) else ''}
 
 def _get_date_context(field):
     date = field.get_value()
-    return {'field_value' : date.strftime("%B %d %Y") if date else ' '}
+    return {'field_value' : date.strftime("%b. %d, %Y") if date else None}
 
 def _get_boolean_context(field):
-    return {'field_value' : 'Yes' if field.get_value() else 'No'}
+    value = field.get_value()
+    return {'field_value' : 'Yes' if value else 'No' if value is not None else None}
 
 def _get_url_context(field):
     url = field.get_value()
     if url:
         return {'url':url, 'field_value':url}
     else:
-        return {'field_value':' '}
+        return {'field_value':None}
     
 def _get_upload_context(field):
     filepath = field.get_value()
@@ -62,7 +70,7 @@ def _get_upload_context(field):
         url = filepath.url if filepath.url else ''
         return {'url':url, 'field_value':filename}
     else:
-        return {'field_value':' '}
+        return {'field_value':None}
 
 def _get_choice_context(field):
     choices = list(Choice.objects.filter(documentation_field=field.documentation_field))
@@ -72,16 +80,19 @@ def _get_choice_context(field):
         field_choices = field.get_value()
     
     # mark all the selected choices
+    at_least_one_selected = False
     for choice in choices:
         if choice in field_choices:
             choice.is_selected = True
+            at_least_one_selected = True  
         else:
             choice.is_selected = False
     
     units = field.documentation_field.units
     
     return {'choices': [ {'choice':c.choice, 'is_selected':c.is_selected} for c in choices ],
-            'units' : units if units else ''
+            'units' : units if units else '',
+            'field_value': (True if at_least_one_selected else None)
            }
     
   
