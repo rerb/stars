4259ea6bae465ed1a0aa9be78fe24abe13ed7886
ben <ben@localhost>
2010-02-05 02:27:03 +0000

    I forgot we had the "date_registered" property for SubmissionSets!!! Using that to order submissions.

--- stars/apps/institutions/templatetags/institution_lists.py
+++ stars/apps/institutions/templatetags/institution_lists.py
@@ -2,15 +2,14 @@ from os.path import basename
 from django import template
 register = template.Library()
 
-from stars.apps.institutions.models import Institution
-from stars.apps.submissions.models import Payment
+from stars.apps.submissions.models import SubmissionSet
 
 @register.inclusion_tag('institutions/tags/latest_registrants.html')
 def show_latest_registrants(count='5'):
     """ Display the (count) most recently registered institutions """
     
     inst_list = []
-    for p in Payment.objects.all().order_by('-id')[0:count]:
-        inst_list.append(p.submissionset.institution)
+    for s in SubmissionSet.objects.all().order_by('-date_registered')[0:count]:
+        inst_list.append(s.institution)
         
     return {'inst_list': inst_list}
