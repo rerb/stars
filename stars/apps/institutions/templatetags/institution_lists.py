8e1d117f44492a09c5aa94d16d96f7c29b545456
ben <ben@localhost>
2010-02-05 01:50:56 +0000

    Automated the list of latest registrants on the home page, per #480.

---
+++ stars/apps/institutions/templatetags/institution_lists.py
@@ -0,0 +1,16 @@
+from os.path import basename
+from django import template
+register = template.Library()
+
+from stars.apps.institutions.models import Institution
+from stars.apps.submissions.models import Payment
+
+@register.inclusion_tag('institutions/tags/latest_registrants.html')
+def show_latest_registrants(count='5'):
+    """ Display the (count) most recently registered institutions """
+    
+    inst_list = []
+    for p in Payment.objects.all().order_by('-date')[0:count]:
+        inst_list.append(p.submissionset.institution)
+        
+    return {'inst_list': inst_list}
