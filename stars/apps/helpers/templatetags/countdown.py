7d0d31fae8d9bfcdba6ac6c4c7660014765e2192
ben <ben@localhost>
2010-08-19 17:30:02 +0000

    Updated countdown to show "Last Day" on home page and then disappear.

--- stars/apps/helpers/templatetags/countdown.py
+++ stars/apps/helpers/templatetags/countdown.py
@@ -8,8 +8,19 @@ deadline = date(year=2010, month=8, day=20)
 
 register = template.Library()
 
-@register.simple_tag
+@register.inclusion_tag('helpers/tags/countdown.html')
 def charter_countdown():
     """ Returns the number of days until the charter participant registration deadline. """
     td = deadline - date.today()
-    return td.days
+    display = False
+    title = None
+    if td.days >= 0:
+        display = True
+    if td.days == 0:
+        title = "Last Day"
+    if td.days == 1:
+        title = "1 day remaining"
+    if td.days > 1:
+        title = "%s days remaining" % td.days
+    
+    return {'display': display, 'title': title}
