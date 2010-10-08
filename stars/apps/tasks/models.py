bbf42bf026fbae45f365e0c27c06aca5bbaca4b6
ben <ben@localhost>
2010-10-08 11:11:23 -0400

    Expanded notification sent_to field.

--- stars/apps/tasks/models.py
+++ stars/apps/tasks/models.py
@@ -11,7 +11,7 @@ class EmailNotification(models.Model):
     EMAIL_TYPES = (('4wk', '4 Weeks Late'),('6mn', "6 months left"))
     
     sent_date = models.DateTimeField(auto_now_add=True)
-    sent_to = models.EmailField()
+    sent_to = models.CharField(max_length=128)
     subject = models.CharField(max_length=64)
     notification_type = models.CharField(max_length=3, choices=EMAIL_TYPES)
     identifier = models.CharField(max_length=8)
