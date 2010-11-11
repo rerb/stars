227459857061aca886dd2a00c1e231c2fd461180
ben <ben@localhost>
2010-11-10 20:10:12 -0500

    Drop tests for CMS, for now.

--- stars/apps/cms/tests/__init__.py
+++ stars/apps/cms/tests/__init__.py
@@ -1,12 +1,1 @@
-from stars.apps.cms.tests.cms_tags import *
 
-__test__ = {}
-from stars.apps.cms.tests.cms_tags import *
-
-__test__ = {}
-from stars.apps.cms.tests.cms_tags import *
-
-__test__ = {}
-from stars.apps.cms.tests.cms_tags import *
-
-__test__ = {}
