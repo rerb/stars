192ea6c7ba9c95b75d1670aed60735551a9a3b9c
ben <ben@localhost>
2010-11-11 11:02:37 -0500

    Added WYSIWG to CMS.

--- stars/apps/cms/admin.py
+++ stars/apps/cms/admin.py
@@ -5,18 +5,27 @@ from stars.apps.cms.models import *
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title','published', )
     prepopulated_fields = {'slug': ('title',)}
+    class Media:
+        js = ('/media/tp/js/tiny_mce/tiny_mce.js',
+              '/media/static/js/textarea_admin.js',)
 admin.site.register(Category, CategoryAdmin)
 
 class SubcategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'published', 'parent')
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('parent',)
+    class Media:
+        js = ('/media/tp/js/tiny_mce/tiny_mce.js',
+              '/media/static/js/textarea_admin.js',)
 admin.site.register(Subcategory, SubcategoryAdmin)
 
 class NewArticleAdmin(admin.ModelAdmin):
     list_display = ('title', 'published', 'created', 'changed', 'stamp')
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('categories','subcategories')
+    class Media:
+        js = ('/media/tp/js/tiny_mce/tiny_mce.js',
+              '/media/static/js/textarea_admin.js',)
 admin.site.register(NewArticle, NewArticleAdmin)
 
 
