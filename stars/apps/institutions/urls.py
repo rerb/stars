4806eee4d8b7ab34abdb901e80c6727ad346a89a
ben <ben@localhost>
2010-12-01 20:41:00 -0500

    Disabled caching on PDF page.

--- stars/apps/institutions/urls.py
+++ stars/apps/institutions/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from stars.apps.institutions.views import *
 from stars.apps.helpers.forms.views import TemplateView
+from django.views.decorators.cache import never_cache
 
 urlpatterns = patterns(
     'stars.apps.institutions.views',
@@ -21,7 +22,7 @@ urlpatterns = patterns(
     (r'^(?P<institution_slug>[^/]+)/report/(?P<submissionset>[^/]+)/inquiry/$', SubmissionInquiryView()),
     
     # PDF Export of Submission
-    (r'^(?P<institution_slug>[^/]+)/report/(?P<submissionset>[^/]+)/pdf/$', PDFExportView(template=None)),
+    (r'^(?P<institution_slug>[^/]+)/report/(?P<submissionset>[^/]+)/pdf/$', never_cache(PDFExportView(template=None))),
 
     # Credit Scorecard
      (r'^(?P<institution_slug>[^/]+)/report/(?P<submissionset>[^/]+)/(?P<category_id>\d+)/(?P<subcategory_id>\d+)/(?P<credit_id>\d+)/$', ScorecardView(template='institutions/scorecards/credit.html')),
