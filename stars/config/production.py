0a76279f8493b0d83b072697ed019f9d88e88fa1
ben <ben@localhost>
2010-12-02 02:21:53 -0500

    Updated production media root to include trailing "/".

--- stars/config/production.py
+++ stars/config/production.py
@@ -23,7 +23,7 @@ ANALYTICS_ID = "UA-1056760-7"
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-MEDIA_ROOT = '/var/www/stars.aashe.org/media'
+MEDIA_ROOT = '/var/www/stars.aashe.org/media/'
 
 SSO_SERVER_URI = WWW_SSO_SERVER_URI
 STARS_DOMAIN = WWW_STARS_DOMAIN
