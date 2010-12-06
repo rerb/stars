deef1cd5ba567c8f645baa12618de8abb9781d46
ben <ben@localhost>
2010-12-06 14:38:21 -0500

    Added new notifications to cron.

--- stars/apps/tasks/notify_cron.py
+++ stars/apps/tasks/notify_cron.py
@@ -10,3 +10,5 @@ from stars.apps.tasks.notifications import *
 send_six_month_notifications(datetime.today())
 send_overdue_notifications(datetime.today())
 send_welcome_email(datetime.today())
+send_post_submission_survey()
+send_sixty_day_notifications()
