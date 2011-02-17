6c8167a0c8e882e4cfbc744940cccd306d3fbc3c
ben <ben@localhost>
2011-02-17 10:36:57 -0500

    Prevent caching of data view.

--- stars/apps/institutions/data_displays/urls.py
+++ stars/apps/institutions/data_displays/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
+from django.views.decorators.cache import never_cache
 
 from stars.apps.institutions.data_displays.views import *
 
@@ -8,5 +9,5 @@ urlpatterns = patterns(
     # data views
     (r'^$', "direct_to_template",{'template': 'institutions/data_views/index.html'}),
     (r'^dashboard/$', Dashboard.as_view()),
-    (r'^categories/$', AggregateFilter.as_view()),
+    (r'^categories/$', never_cache(AggregateFilter.as_view())),
 )
