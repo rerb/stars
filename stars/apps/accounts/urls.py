0aa9e3c35518a74dcd69df66cdfc8348626eeda0
ben <ben@localhost>
2011-02-24 19:02:51 -0500

    Fixed issue with logout.

--- stars/apps/accounts/urls.py
+++ stars/apps/accounts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = patterns(
     
     #(r'^login/$', 'django.contrib.accounts.views.login', {'template_name': 'auth/login.html'}),
     (r'^login/$', 'stars.apps.accounts.views.login'),
-    (r'^logout/$', 'django.contrib.accounts.views.logout_then_login'),
+    (r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
     
     # Terms of Service
     (r'^tos/$', 'stars.apps.accounts.views.terms_of_service'),
