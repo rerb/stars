4f353bf9063337eaf9858ee636eabd7908a01c9c
ben <ben@localhost>
2011-03-23 11:19:54 -0400

    Added a `clean_watchdog` management method;

---
+++ stars/apps/helpers/management/commands/clean_watchdog.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand, CommandError
+
+from stars.apps.tool.admin.watchdog.models import WatchdogEntry
+
+from datetime import datetime, timedelta
+
+class Command(BaseCommand):
+    args = '<none>'
+    help = 'Clears all entries older than a 30 days'
+
+    def handle(self, *args, **options):
+        
+        td = timedelta(days=30)
+        d = datetime.now() - td
+        
+        qs = WatchdogEntry.objects.filter(timestamp__lt=d)
+        
+        count = qs.count()
+        qs.delete()
