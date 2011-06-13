e88322a3eb00dae6178dc364f9751aaa5fcbb9e8
jamstooks <jamstooks@localhost>
2011-06-13 16:17:09 -0400

    Updates to test runs for certificate generation;

--- stars/tests/certificate.py
+++ stars/tests/certificate.py
@@ -3,16 +3,19 @@
 from stars.apps.institutions.models import *
 from stars.apps.submissions.models import SubmissionSet
 from stars.apps.submissions.tasks import send_certificate_pdf
+from stars.apps.submissions.pdf.export import build_certificate_pdf
 
 import sys
 
-#outfile = "certificate.pdf"
-
-#pdf = build_certificate_pdf(ss)
-#f = open(outfile, 'w')
-#f.write(pdf.getvalue())
-
 ss = SubmissionSet.objects.get(pk=60)
-print >> sys.stdout, "Running cert for %s" % ss
-send_certificate_pdf.delay(ss)
-print >> sys.stdout, "Done"
+
+outfile = "certificate.pdf"
+
+pdf = build_certificate_pdf(ss)
+f = open(outfile, 'w')
+f.write(pdf.getvalue())
+
+# ss = SubmissionSet.objects.get(pk=60)
+# print >> sys.stdout, "Running cert for %s" % ss
+# send_certificate_pdf.delay(ss)
+# print >> sys.stdout, "Done"
--- stars/tests/send_certificate_list.py
+++ stars/tests/send_certificate_list.py
@@ -3,11 +3,16 @@
 from stars.apps.submissions.models import SubmissionSet
 from stars.apps.submissions.tasks import send_certificate_pdf
 
+from django.conf import settings
+
 import sys
 
 ss_id_list = [
-    430, 139, 300, 266, 131, 100, 280, 61, 283, 53, 278, 277, 122, 281
+    283, 430
 ]
+
+settings.EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+settings.CELERY_ALWAYS_EAGER = True
 
 for ss_id in ss_id_list:
     ss = SubmissionSet.objects.get(pk=ss_id)
