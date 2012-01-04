95934364402e59d4bce36700e2076a04b45e7c6b
jamstooks <jamstooks@localhost>
2012-01-04 11:51:22 -0500

    Removed old tests after moving to migrations app;

--- stars/apps/credits/tests/__init__.py
+++ stars/apps/credits/tests/__init__.py
@@ -3,7 +3,6 @@ from incremental_features import TestIncrementalFeatures
 from utils import TestUtils
 
 __test__ = {
-    'TestMigration': TestMigration,
     'TestIncrementalFeatures': TestIncrementalFeatures,
     'TestUtils': TestUtils,
 }
