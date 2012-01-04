0ba821c8867f31955dffcd0b8f1f8100327688c3
jamstooks <jamstooks@localhost>
2012-01-04 15:32:33 -0500

    Remove relics of the test rules app;

--- stars/apps/rules/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-from stars.apps.rules.views import OrderPizzaView
-
-urlpatterns = patterns(
-    '',
-
-    (r'^order_pizza/(?P<topping>\w+)/(?P<charge>\d+)/$', OrderPizzaView.as_view()),
-)
