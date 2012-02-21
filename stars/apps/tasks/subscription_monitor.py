95d309ef99e0490b99e550cf92c4257a8dc0f3e4
jamstooks <jamstooks@localhost>
2012-02-21 11:26:47 -0500

    Added subscription update cron;

--- stars/apps/tasks/subscription_monitor.py
+++ stars/apps/tasks/subscription_monitor.py
@@ -3,6 +3,8 @@ import sys
 
 from stars.apps.institutions.models import Institution
 from stars.apps.migrations.utils import create_ss_mirror
+from stars.apps.registration.views import init_submissionset
+from stars.apps.helpers import watchdog
 
 def update_institution_properties():
     """
@@ -12,39 +14,35 @@ def update_institution_properties():
             current_rating
     """
     for i in Institution.objects.all():
+#        print >> sys.stdout, "Checking %s" % i.name
         # if their subscription has expired
         if i.current_subscription and i.current_subscription.end_date < datetime.date.today():
-            print >> sys.stdout, "Expired Subscription: %s" % i
-            print >> sys.stdout, i.current_subscription.end_date
+            print >> sys.stdout, "Subscription Expired on %s for %s" % (i.current_subscription.end_date, i)
+            
+            watchdog.log("Sub_cron", "Subscription Expired for %s" % i, watchdog.NOTICE)
             i.current_subscription = None
             i.is_participant = False
-        # if they don't have a subscription (this isn't likely, but still possible)
-        if i.current_subscription == None:
-            print >> sys.stdout, "Expired Participant: %s" % i
-            i.is_participant = False
-        # once their rating has expired
-        if i.rating_expires and i.rating_expires < datetime.date.today():
-            print >> sys.stdout, "Expired Rating: %s" % i
-            i.rating_expires = None
-            i.rating = None
-        i.save()
-        
-def update_submissions():
-    """
-        Create new submissions when:
-            - an institution's current submission is rated
-    """
-    for i in Institution.objects.all():
-        old_ss = None
-        if i.current_submission and i.current_submission.status == 'r':
-            old_ss = i.current_submission
-        elif i.current_submission == None:
-            old_ss = i.get_latest_rated_submission()
-        if old_ss:
-            new_ss = create_ss_mirror(old_ss)
-            new_ss.is_locked = False
-            new_ss.is_visible = True
-            new_ss.save()
-            i.current_submission = new_ss
+            # @todo: send email
+            
+            # check to see if there are any current subscriptions
+            if i.current_subscription == None:
+                for s in i.subscription_set.all():
+                    if s.start_date < datetime.date.today() and s.end_date > datetime.date.today():
+                        watchdog.log("Sub_cron", "New Subscription found for %s" % i, watchdog.NOTICE)
+                        print >> sys.stdout, "Renewal Found."
+                        i.current_subscription = s
+                        i.is_participant = True
+            
+            # if there is still no subscription create an empty submission
+            # to be the current submission
+            if i.current_subscription == None:
+                try:
+                    u = i.current_submission.registering_user
+                    i.current_submission = init_submissionset(i, u)
+                except SubmissionSet.DoesNotExist:
+                    print >> sys.stderr, "No Current Submission found for %s" % i
+                    watchdog.log("Sub_cron", "No Current Submission for %s" % i, watchdog.ERROR)
+            
             i.save()
             
+update_institution_properties()
