88e637883328b9d777495b8e05915d0451a4f562
jamstooks <jamstooks@localhost>
2012-03-27 13:53:40 -0400

    Updated logic for moving reg reasons from payments to subscriptions;

--- stars/apps/registration/views.py
+++ stars/apps/registration/views.py
@@ -371,6 +371,12 @@ def init_subscription(institution, amount_due, date_callback=date.today):
                                 end_date=deadline,
                                 amount_due=amount_due,
                                 paid_in_full=(amount_due==0))
+    if institution.international:
+        subscription.reason = "international"
+    elif institution.is_member:
+        subscription.reason = "member_reg"
+    else:
+        subscription.reason = "nonmember_reg"
     subscription.save()
     return subscription
 
@@ -406,17 +412,9 @@ def register_institution(user, institution, payment_type, price, payment_dict):
     institution.current_subscription = subscription
     institution.set_active_submission(submissionset)
     institution.save()
-    
-    # Save Payment
-    if institution.is_member_institution():
-        reason = "member_reg"
-    elif institution.international:
-        reason = "international"
-    else:
-        reason = "nonmember_reg"
         
     if payment_dict:
-        payment = SubscriptionPayment(subscription=subscription, date=date.today(), amount=price, user=user, reason=reason, method=payment_type, confirmation="none")
+        payment = SubscriptionPayment(subscription=subscription, date=date.today(), amount=price, user=user, method=payment_type, confirmation="none")
         payment.save()
     
     # Primary Contact
