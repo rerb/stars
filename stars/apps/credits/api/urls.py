0c3eb5b786d5350678a1bb341dc1a90e60d1e93f
Bob Erb <bob.erb@aashe.org>
2012-06-15 12:29:15 -0400

    Moved credits api URLs definitions into credits.api.urls.

--- stars/apps/credits/api/urls.py
+++ stars/apps/credits/api/urls.py
@@ -2,10 +2,11 @@ from tastypie.api import Api
 from stars.apps.credits.api import resources
 
 v1_api = Api(api_name='v1')
-v1_api.register(resources.CategoryResource())
-v1_api.register(resources.CreditResource())
-v1_api.register(resources.CreditSetResource())
-v1_api.register(resources.DocumentationFieldResource())
-v1_api.register(resources.SubcategoryResource())
+for model in (resources.CategoryResource,
+              resources.CreditResource,
+              resources.CreditSetResource,
+              resources.DocumentationFieldResource,
+              resources.SubcategoryResource):
+    v1_api.register(model())
 
 urlpatterns = v1_api.urls
--- stars/apps/credits/urls.py
+++ stars/apps/credits/urls.py
@@ -1,20 +1,12 @@
 from django.conf.urls.defaults import include, patterns
 from django.contrib import admin
+
+from stars.apps.credits.api.urls import v1_api
+
 admin.autodiscover()
 
 import aashe_rules
 aashe_rules.autodiscover()
-
-from tastypie.api import Api
-from stars.apps.credits.api.resources import *
-
-v1_api = Api(api_name='v1')
-v1_api.register(CategoryResource())
-v1_api.register(CreditResource())
-v1_api.register(CreditSetResource())
-v1_api.register(DocumentationFieldResource())
-v1_api.register(IncrementalFeatureResource())
-v1_api.register(SubcategoryResource())
 
 urlpatterns = patterns('',
     (r'v1/credits/', include(v1_api.urls)),
