c364a6d8a50dad2b5b1d61b61e2af7c2d9ce6ab5
Bob Erb <bob.erb@aashe.org>
2012-06-21 01:19:33 -0400

    I like loops!

--- stars/apps/submissions/newapi/urls.py
+++ stars/apps/submissions/newapi/urls.py
@@ -7,13 +7,7 @@ for model in (resources.SubmissionSetResource,
               resources.CategorySubmissionResource,
               resources.SubcategorySubmissionResource,
               resources.CreditSubmissionResource,
-              # resources.DocumentationFieldResource,
               ):
     v1_api.register(model())
 
-# v1_api.register(resources.CategoryResource())
-# v1_api.register(resources.CreditResource())
-# v1_api.register(resources.CreditSetResource())
-# v1_api.register(resources.DocumentationFieldResource())
-# v1_api.register(resources.SubcategoryResource())
 urlpatterns = v1_api.urls
