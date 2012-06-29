00f3d49d3c75cd4b800f6ee8ee775b2102adcb2f
Bob Erb <bob.erb@aashe.org>
2012-06-29 15:13:23 -0400

    Don't show unrated submissions for institutions.

--- stars/apps/institutions/api/resources.py
+++ stars/apps/institutions/api/resources.py
@@ -4,6 +4,7 @@
 from tastypie import fields
 
 from stars.apps.institutions import models
+from stars.apps.submissions.models import SubmissionSet
 from stars.apps.api.resources import StarsApiResource
 from stars.apps.api.paths import SUBMISSIONS_RESOURCE_PATH
 
@@ -23,3 +24,10 @@ class InstitutionResource(StarsApiResource):
         # @todo: need aashe_id and/or id?
         fields = ['name', 'aashe_id', 'id']
         allowed_methods = ['get']
+
+    def dehydrate_submission_sets(self, bundle):
+        """Filter unrated submission sets."""
+        for submission in bundle.data['submission_sets']:
+            if submission not in SubmissionSet.objects.get_rated():
+                del(submission)
+        return bundle.data['submission_sets']
