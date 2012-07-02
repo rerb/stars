c3efa937a065717538a3a8266f62e921113a86af
Matt <mthomas@aashe.org>
2012-07-02 09:27:15 -0400

    adding myself to settings

---
+++ docs/api/access.rst
@@ -0,0 +1,39 @@
+.. _stars-access-levels:
+
+STARS Access Levels & Permissions
+=================================
+
+Access Levels Overview Table
+----------------------------
+
+The table below provides an overview of the access levels available in
+the STARS API. 
+
+
++---------------+---------------------+---------------+
+|Report Type    |Notes                |Access         |
+|               |                     |               |
+|               |                     |               |
+|               |                     |               |
+|               |                     |               |
+|               |                     |               |
++===============+=====================+===============+
+|STARS Reports  |These are public, so |READ ONLY      |
+|               |data from their      |               |
+|               |reports will be made |               |
+|               |available to anyone  |               |
+|               |with access to the   |               |
+|               |API.                 |               |
++---------------+---------------------+---------------+
+|In Progress    |Access to            |READ WRITE     |
+|Submissions    |third-parties granted|(granted by    |
+|               |specifically by the  |institution)   |
+|               |institution. All     |               |
+|               |institutions will    |               |
+|               |have access to their |               |
+|               |own submissions.     |               |
++---------------+---------------------+---------------+
+|STARS-Lite     |Access granted to    |READ ONLY      |
+|Submissions    |Common Data Set      |               |
+|               |group.               |               |
++---------------+---------------------+---------------+
---
+++ stars/apps/helpers/tests/helper_tags.py
@@ -0,0 +1,40 @@
+"""
+    Test Suite for Helpers.  
+    These tests cover all of the helpers, including
+     - form helpers
+     - exceptions
+     - widgets
+     - xmlrpc client
+     - template tags
+"""
+from django.test import TestCase
+
+from stars.apps.helpers.templatetags import help
+class Help_tags_Test(TestCase):
+        
+    """
+        #######   help.py Test Suite.  #######
+    """
+    def test_show_help_context_as_tooltip(self):
+        context = help.show_help_context("test")
+        # test help text is:  This 'help context' is used by "automated unit tests" - do not change this text!
+        self.assertTrue(context['tooltip'])
+        self.assertEqual(context['help_text'], "This \&#39;help context\&#39; is used by \&quot;automated unit tests\&quot; - do not change this text!")
+
+    def test_show_help_context_as_inline(self):
+        context = help.show_help_context("test", False)
+        # test help text is:  This 'help context' is used by "automated unit tests" - do not change this text!
+        self.assertFalse(context['tooltip'])
+        self.assertEqual(context['help_text'], "This \'help context\' is used by \"automated unit tests\" - do not change this text!")
+
+    def test_show_help_text_as_tooltip(self):
+        context = help.show_help_text("This 'help context' is used by \"automated unit tests\" - do not change this text!")
+        # test help text is:  This 'help context' is used by "automated unit tests" - do not change this text!
+        self.assertTrue(context['tooltip'])
+        self.assertEqual(context['help_text'], "This \\&#39;help context\\&#39; is used by \\&quot;automated unit tests\\&quot; - do not change this text!")
+
+    def test_show_help_text_as_inline(self):
+        context = help.show_help_text("This 'help context' is used by \"automated unit tests\" - do not change this text!", False)
+        # test help text is:  This 'help context' is used by "automated unit tests" - do not change this text!
+        self.assertFalse(context['tooltip'])
+        self.assertEqual(context['help_text'], "This \'help context\' is used by \"automated unit tests\" - do not change this text!")
