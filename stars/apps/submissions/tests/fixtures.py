cebc3de209879c575e447fb599ed63c7cefbb0c8
Bob Erb <bob.erb@aashe.org>
2012-07-09 09:13:31 -0400

    Added tests for more classes.

--- stars/apps/submissions/tests/fixtures.py
+++ stars/apps/submissions/tests/fixtures.py
@@ -1,13 +1,42 @@
-from django.test import TestCase
+from stars.apps.api.test import StarsApiTestCase
+from stars.apps.credits.models import *
+from stars.apps.submissions.models import *
+from stars.apps.institutions.models import Institution
 
-from stars.apps.credits.models import CreditSet
-from stars.apps.submissions.models import SubmissionSet
+class FixturesTest(StarsApiTestCase):
 
-class FixturesTest(TestCase):
-    fixtures = ['test_api_creditset.json', 'test_api_submissionset.json']
+    def testIncrementalFeatureLoad(self):
+        self.assertTrue(IncrementalFeature.objects.all() > 0)
 
-    def setUp(self):
-        pass
+    def testRatingLoad(self):
+        self.assertTrue(Rating.objects.all() > 0)
+
+    def testInstitutionLoad(self):
+        self.assertTrue(Institution.objects.all() > 0)
+
+    def testCategoryLoad(self):
+        self.assertTrue(Category.objects.all() > 0)
+
+    def testSubcategoryLoad(self):
+        self.assertTrue(Subcategory.objects.all() > 0)
+
+    def testCreditLoad(self):
+        self.assertTrue(Credit.objects.all() > 0)
+
+    def testDocumentationFieldLoad(self):
+        self.assertTrue(DocumentationField.objects.all() > 0)
+
+    def testCategorySubmissionLoad(self):
+        self.assertTrue(CategorySubmission.objects.all() > 0)
+
+    def testSubcategorySubmissionLoad(self):
+        self.assertTrue(SubcategorySubmission.objects.all() > 0)
+
+    def testCreditSubmissionLoad(self):
+        self.assertTrue(CreditSubmission.objects.all() > 0)
+
+    def testDocumentationfieldSubmissionLoad(self):
+        self.assertTrue(NumericSubmission.objects.all() > 0)
 
     def testCreditSetLoad(self):
         self.assertTrue(CreditSet.objects.all() > 0)
