ef31a20a5b5fa2a8e3e564b6f97fd204d2a35fb1
Bob Erb <bob.erb@aashe.org>
2012-07-13 12:11:11 -0400

    Added tests for search/filtering.

--- stars/apps/institutions/api/test.py
+++ stars/apps/institutions/api/test.py
@@ -34,3 +34,76 @@ class InstitutionResourceTestCase(StarsApiTestCase):
             for submission_set in institution['submission_sets']:
                 self.assertFalse(
                     submission_set['rating'] in ['None', None, ''])
+
+
+    # These are the names of the institutions that are
+    # loaded via api_test_fixture.json.  Note that if
+    # other institutions are loaded, these search tests
+    # will probably fail and need to be adjusted.
+    # For now, they expect the following names, and no
+    # others.
+    #
+    # "Moraine Valley Community College"
+    # "Portland State University"
+    # "Western University"
+    # "University of Minnesota, Duluth"
+
+    def test_search_name_contains(self):
+        """Test the 'name_contains' filter for searching."""
+        path = self.list_path + '?name_contains=iversi'
+        resp = self.get(path)
+        payload = json.loads(resp.content)
+        self.assertTrue(payload['meta']['total_count'] is 3)
+
+    def test_search_name_contains_case_insensitivity(self):
+        """The 'name_contains' filter should be case insensitive."""
+        path = self.list_path + '?name_contains=IVERSI'
+        resp = self.get(path)
+        payload = json.loads(resp.content)
+        self.assertTrue(payload['meta']['total_count'] is 3,
+                        'case insensitive match failed')
+
+    def test_search_name_startswith(self):
+        """Test the 'name_startswith' filter for searching."""
+        path = self.list_path + '?name_startswith=Wes'
+        resp = self.get(path)
+        payload = json.loads(resp.content)
+        self.assertTrue(payload['meta']['total_count'] is 1)
+
+    def test_search_name_startswith_case_insensitivity(self):
+        """The 'name_startswith' filter should be case insensitive."""
+        path = self.list_path + '?name_startswith=wes'
+        resp = self.get(path)
+        payload = json.loads(resp.content)
+        self.assertTrue(payload['meta']['total_count'] is 1,
+                        'case insensitive match failed')
+
+    def test_search_name_endswith(self):
+        """Test the 'name_endswith' filter for searching."""
+        path = self.list_path + '?name_endswith=sity'
+        resp = self.get(path)
+        payload = json.loads(resp.content)
+        self.assertTrue(payload['meta']['total_count'] is 2)
+
+    def test_search_name_endswith_case_insensitivity(self):
+        """The 'name_endswith' filter should be case insensitive."""
+        path = self.list_path + '?name_endswith=SITY'
+        resp = self.get(path)
+        payload = json.loads(resp.content)
+        self.assertTrue(payload['meta']['total_count'] is 2,
+                        'case insensitive match failed')
+
+    def test_search_name(self):
+        """Test the 'name' filter for searching."""
+        path = self.list_path + '?name=Moraine Valley Community College'
+        resp = self.get(path)
+        payload = json.loads(resp.content)
+        self.assertTrue(payload['meta']['total_count'] is 1)
+
+    def test_search_name_case_insensitivity(self):
+        """The 'name' filter should be case insensitive."""
+        path = self.list_path + '?name=MORAINE VALLEY COMMUNITY COLLEGE'
+        resp = self.get(path)
+        payload = json.loads(resp.content)
+        self.assertTrue(payload['meta']['total_count'] is 1,
+                        'case insensitive match failed')
