a94fafc7b841026aea9bf6dbac7d7a62d400b70a
Bob Erb <bob.erb@aashe.org>
2012-07-13 11:01:05 -0400

    Added wrappers around PATCH, POST, PUT, and DELETE HTTP methods.

--- stars/apps/api/test.py
+++ stars/apps/api/test.py
@@ -26,15 +26,40 @@ class StarsApiTestCase(ResourceTestCase):
         if self.created_user:
             User.objects.get(pk=self.user.id).delete()
 
+    # HTTP method wrappers:
+
+    def delete(self, path):
+        """Do a DELETE using the default credentials."""
+        return self.api_client.delete(path,
+                                      authentication=self.get_credentials())
+
     def get(self, path):
         """Do a GET using the default credentials."""
         return self.api_client.get(path,
                                    authentication=self.get_credentials())
 
-    def requires_auth(self, path):
-        """Does path require auth?"""
-        resp = self.api_client.get(path)
-        self.assertHttpUnauthorized(resp)
+    def patch(self, path, data=None):
+        """Do a PATCH using the default credentials."""
+        data = data or {}
+        return self.api_client.patch(path,
+                                     authentication=self.get_credentials(),
+                                     data=data)
+
+    def post(self, path, data=None):
+        """Do a POST using the default credentials."""
+        data = data or {}
+        return self.api_client.post(path,
+                                    authentication=self.get_credentials(),
+                                    data=data)
+
+    def put(self, path, data=None):
+        """Do a PUT using the default credentials."""
+        data = data or {}
+        return self.api_client.put(path,
+                                   authentication=self.get_credentials(),
+                                   data=data)
+
+    # Misc.
 
     def assertValidJSONResponseNotError(self, response):
         """Response is valid JSON and not an error message."""
@@ -45,3 +70,8 @@ class StarsApiTestCase(ResourceTestCase):
     def get_credentials(self):
         return self.create_apikey(username=self.user.username,
                                   api_key=self.user.api_key.key)
+
+    def requires_auth(self, path):
+        """Does path require authentication?"""
+        resp = self.api_client.get(path)
+        self.assertHttpUnauthorized(resp)
