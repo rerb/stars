518536452032ef8b2a3cb52661a9d6f1fd00050d
Bob Erb <bob.erb@aashe.org>
2012-07-16 11:41:40 -0400

    Added coverage HTML reports and emacs temp files to .hgignore.

---
+++ stars/apps/registration/tests/test_survey.py
@@ -0,0 +1,51 @@
+"""
+    Doctests for the survey view after registration
+
+    Test premises:
+        - the forms are processed correctly
+            - Form loads
+            - Form processes
+            - Saves survey
+            - View redirects
+"""
+
+from django.test import TestCase
+from django.test.client import Client
+
+from stars.apps.institutions.models import *
+
+from datetime import date
+import sys, os
+
+class TestSurvey(TestCase):
+    fixtures = ['submit_for_rating_tests.json','registration_tests.json']
+
+    def setUp(self):
+        pass
+        
+    def testSurveyView(self):
+        """
+            Test the RegistrationSurveyView
+                - Handles a basic HTTP request w/out 500
+                - Processes the form and returns a redirect account
+                - Saves survey
+        """
+        self.assertTrue(RegistrationSurvey.objects.count() == 0)
+        
+        c = Client()
+        c.login(username='test_user', password='test')
+        post_dict = {}
+        response = c.get('/register/survey/', post_dict)
+        self.assertTrue(response.status_code == 200)
+        
+        post_dict = {
+            'institution': 1,
+            'user': 5215,
+            'source': 'heard',
+            'reasons': [1,],
+            'other': 'other',
+            'primary_reason': 1,
+            'enhancements': 'enhance!',
+        }
+        response = c.post('/register/survey/', post_dict, follow=False)
+        self.assertTrue(response.status_code == 302)
---
+++ stars/config/bob.py
@@ -0,0 +1,109 @@
+"""
+    Personal config file for development in Bob's local environment.
+"""
+import os
+
+from settings import *
+
+HIDE_REPORTING_TOOL = False
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+DEBUG_TOOLBAR = False
+MAINTENANCE_MODE = False
+CELERY_ALWAYS_EAGER = True
+
+ADMINS = ('bob.erb@aashe.org',)
+MANAGERS = ADMINS
+
+def get_api_test_mode():
+    try:
+        return int(os.environ['API_TEST_MODE'])
+    except KeyError:
+        if 'test' in sys.argv:
+            return False  # Unintuitive, isn't it? Should rename to AUTH_ON.
+        else:
+            return True  # If True, auth is turned off
+
+API_TEST_MODE = get_api_test_mode()
+
+def use_sqlite_for_tests():
+    try:
+        return os.environ['USE_SQLITE_FOR_TESTS']
+    except KeyError:
+        return True
+
+if (('test' in sys.argv) or ('testserver' in sys.argv)):
+    if use_sqlite_for_tests():
+        DATABASES = {
+            'default': {
+                'NAME': '/Users/rerb/sqlite/stars.db',
+                'ENGINE': 'django.db.backends.sqlite3',
+                'USER': 'root',
+                'PASSWORD': '',
+                'HOST': 'localhost',
+                },
+                'iss': {
+                    'NAME': '/Users/rerb/sqlite/iss.db',
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'USER': 'root',
+                    'PASSWORD': '',
+                    'HOST': 'localhost',
+                }
+        }
+    else:
+        DATABASES = {
+            'default': {
+                'NAME': 'stars-test',
+                'ENGINE': 'django.db.backends.mysql',
+                'STORAGE_ENGINE': 'MyISAM',
+                'USER': 'root',
+                'PASSWORD': '',
+                'HOST': 'localhost',
+                'OPTIONS': {
+                    "connect_timeout": 30,
+                    },
+                },
+            'iss': {
+                'NAME': 'iss-test',
+                'ENGINE': 'django.db.backends.mysql',
+                'USER': 'root',
+                'PASSWORD': '',
+                'HOST': 'localhost',
+                }
+        }
+
+else:
+    DATABASES = {
+        'default': {
+            'NAME': 'stars',
+            'ENGINE': 'django.db.backends.mysql',
+            'STORAGE_ENGINE': 'MyISAM',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'OPTIONS': {
+                "connect_timeout": 30,
+                },
+            },
+        'iss': {
+            'NAME': 'iss',
+            'ENGINE': 'django.db.backends.mysql',
+            'USER': 'root',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            }
+    }
+
+
+DATABASE_ROUTERS = ('aashe.issdjango.router.ISSRouter',)
+
+# Stand alone mode indicates that the server will be running using
+# the django dev server so we will need to serve the static files (see urls.py)
+STANDALONE_MODE = True
+
+INSTALLED_APPS += ('django_nose',
+                   'fixture_magic')
+
+if 'TEST_RUNNER' in os.environ:
+    if os.environ['TEST_RUNNER']:  # only use it if there's a value set
+        TEST_RUNNER = os.environ['TEST_RUNNER'] or TEST_RUNNER
