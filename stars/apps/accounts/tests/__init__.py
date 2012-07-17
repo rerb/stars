40fbad8ee237d1abe3e14cbaff0679b44919213a
Bob Erb <bob.erb@aashe.org>
2012-07-17 10:37:39 -0400

    Removed reference to rm'ed test perm_mixin.

--- stars/apps/accounts/tests/__init__.py
+++ stars/apps/accounts/tests/__init__.py
@@ -1,6 +1,5 @@
-import perm_mixin, submission_mixin, account_mixin, users
+import submission_mixin, account_mixin, users
 __test__ = {
-    'perm': perm_mixin,
     'submission': submission_mixin,
     'account': account_mixin,
     'users': users,
