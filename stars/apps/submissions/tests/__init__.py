975b95a9f08c2ab00a046d33d317d563438c3808
Bob Erb <bob.erb@aashe.org>
2012-07-18 14:47:34 -0400

    Rm'ed import of extensions.ExtensionTest from submissions/tests/__init__.py. submissions/tests/extensions.py rm'ed in an earlier changeset.

--- stars/apps/submissions/tests/__init__.py
+++ stars/apps/submissions/tests/__init__.py
@@ -1,6 +1,5 @@
 from stars.apps.submissions.tests import scoring
 from submission_manager import ManagerTest
-from extensions import ExtensionTest
 from data_corrections import DataCorrectionTest
 #from pdf import PDFTest
 
