d015f600f665e8e71571faa74e9eabd4440c53b6
Bob Erb <bob.erb@aashe.org>
2012-07-19 16:12:19 -0400

    Changes to tests and fixture for tool/my_submission/tests/submit_for_rating.py.

--- stars/apps/tool/my_submission/tests/submit_for_rating.py
+++ stars/apps/tool/my_submission/tests/submit_for_rating.py
@@ -9,6 +9,8 @@
             - Finalize
         - email notifications are sent
 """
+import tempfile
+
 from django.test import TestCase
 from django.core import mail
 from django.test.client import Client
@@ -16,10 +18,11 @@ from django.conf import settings
 
 from stars.apps.submissions.models import SubmissionSet
 
-import sys, os
 
 class RatingTest(TestCase):
-    fixtures = ['submit_for_rating_tests.json','notification_emailtemplate_tests.json']
+
+    fixtures = ['submit_for_rating_tests.json',
+                'notification_emailtemplate_tests.json']
 
     def setUp(self):
 
@@ -33,6 +36,8 @@ class RatingTest(TestCase):
         c = Client()
         c.login(username='test_user', password='test')
 
+        import pdb; pdb.set_trace()
+
         self.confirmView(c)
         self.letterView(c)
         self.finalizeView(c)
@@ -44,16 +49,7 @@ class RatingTest(TestCase):
                 - Handles a basic HTTP request w/out 500
                 - Processes the form and returns a redirect to step 2
         """
-
-        print >> sys.stderr, "TESTING: Confirm Submission"
-
-        post_dict = {}
-        response = c.get('/tool/submissions/submit/', post_dict)
-        print response.status_code
-        self.assertTrue(response.status_code == 200)
-
-        post_dict = {'submission_boundary': 'boundary text',}
-        response = c.post('/tool/submissions/submit/', post_dict, follow=False)
+        response = c.get('/tool/submissions/submit/')
         self.assertTrue(response.status_code == 302)
 
     def letterView(self, c):
@@ -63,28 +59,30 @@ class RatingTest(TestCase):
                 - Processes the form and redirects to step 3
         """
 
-        print >> sys.stderr, "TESTING: Letter"
-
-        post_dict = {}
-        response = c.get('/tool/submissions/submit/letter/', post_dict)
+        response = c.get('/tool/submissions/submit/letter/')
         self.assertTrue(response.status_code == 200)
 
-        f = open(os.path.join(os.path.dirname(__file__), 'test.pdf'))
+        pdf_file = tempfile.TemporaryFile()
+        letter_file = tempfile.TemporaryFile()
+        letter_file.write('Some text just for the hell of it')
+        letter_file.seek(0)
 
         post_dict = {
-                        'letter_form-presidents_letter': f,
-                        'exec_contact_form-executive_contact_first_name': 'First',
-                        'exec_contact_form-executive_contact_last_name': 'Last',
-                        'exec_contact_form-executive_contact_title': 'Title',
-                        'exec_contact_form-executive_contact_department': 'Dept.',
-                        'exec_contact_form-executive_contact_email': 'test@test.edu',
-                        'exec_contact_form-executive_contact_address': 'Address',
-                        'exec_contact_form-executive_contact_city': 'City',
-                        'exec_contact_form-executive_contact_state': 'ST',
-                        'exec_contact_form-executive_contact_zip': '12345',
-                        'exec_contact_form-confirm': 'on'
-                    }
-        response = c.post('/tool/submissions/submit/letter/', post_dict, follow=False)
+            'letter_form-presidents_letter': pdf_file,
+            'exec_contact_form-president_first_name': 'First',
+            'exec_contact_form-president_last_name': 'Last',
+            'exec_contact_form-president_title': 'Title',
+            'exec_contact_form-department': 'Dept.',
+            'exec_contact_form-email': 'test@test.edu',
+            'exec_contact_form-president_address': 'Address',
+            'exec_contact_form-president_city': 'City',
+            'exec_contact_form-president_state': 'ST',
+            'exec_contact_form-president_zip': '12345',
+            'exec_contact_form-confirm': 'on',
+            'letter_form-presidents_letter': letter_file
+            }
+        response = c.post('/tool/submissions/submit/letter/', post_dict,
+                          follow=False)
 
         self.assertTrue(response.status_code == 302)
 
@@ -95,8 +93,6 @@ class RatingTest(TestCase):
                 - Processes the form and saves the SubmissionSet object
                 - Sends emails to liason, user and stars staff
         """
-
-        print >> sys.stderr, "TESTING: Finalize"
 
         post_dict = {}
         response = c.get('/tool/submissions/submit/finalize/', post_dict)
