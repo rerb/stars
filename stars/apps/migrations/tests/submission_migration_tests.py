3ad174e10220c668a51df1d994438ad4abb6ade5
Bob Erb <bob.erb@aashe.org>
2012-07-26 15:23:25 -0400

    Fixed VersionMigrationTest in migrations/tests/submission_migration_tests.py.

--- stars/apps/migrations/tests/submission_migration_tests.py
+++ stars/apps/migrations/tests/submission_migration_tests.py
@@ -7,6 +7,8 @@
      - Don't migrate 1.1 to 1.1
 
 """
+from datetime import datetime
+
 from django.test import TestCase
 from django.core import mail
 from django.test.client import Client
@@ -16,12 +18,11 @@ from stars.apps.migrations.utils import migrate_submission
 from stars.apps.submissions.utils import init_credit_submissions
 from stars.apps.submissions.models import *
 
-import sys
 
-from datetime import datetime
 
 class VersionMigrationTest(TestCase):
-    fixtures = ['submission_migration_test.json','notification_emailtemplate_tests.json']
+    fixtures = ['submission_migration_test.json',
+                'notification_emailtemplate_tests.json']
 
     def setUp(self):
 
@@ -37,14 +38,14 @@ class VersionMigrationTest(TestCase):
         c.login(username='tester', password='test')
 
         # Just check the migration page returns a 200 response
-        response = c.get('/tool/manage/submissionsets/1/migrate/')
+        response = c.get('/tool/manage/migrate/')
         self.assertEqual(response.status_code, 200)
 
         # Now submit for a migration
         post_dict = {
             "1-is_locked": "on"
         }
-        response = c.post('/tool/manage/submissionsets/1/migrate/', post_dict)
+        response = c.post('/tool/manage/migrate/version/', post_dict)
         self.assertEqual(response.status_code, 302)
 
         # Check that the data was migrated correctly
