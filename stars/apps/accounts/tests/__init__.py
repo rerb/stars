5d21d35c2a8f996b3563650ee92b1004c483c975
Bob Erb <bob.erb@aashe.org>
2012-07-27 10:11:14 -0400

    Rm'ed SubmissionMixinTest from accounts/tests. SubmissionMixinTest was removed in a recent changeset because the logic it tests has been moved from SubmissionSet to Subscription.

--- stars/apps/accounts/tests/__init__.py
+++ stars/apps/accounts/tests/__init__.py
@@ -1,7 +1,5 @@
 import account_mixin, users
-from submission_mixin import SubmissionMixinTest
 __test__ = {
-    'submission': SubmissionMixinTest,
     'account': account_mixin,
     'users': users,
 }
