6a639d84c6d519a3404854039d57209a6e86539e
Bob Erb <bob.erb@aashe.org>
2012-07-27 14:34:39 -0400

    Made exec contact middle name optional in RegistrationForm.

--- stars/apps/registration/forms.py
+++ stars/apps/registration/forms.py
@@ -15,7 +15,7 @@ class WriteInInstitutionForm(forms.Form):
         A form for an institution that we don't have stored yet
     """
     institution_name = forms.CharField(max_length=255, required=True)
-    
+
     def __init__(self, *args, **kwargs):
         super(WriteInInstitutionForm, self).__init__(*args, **kwargs)
         self.fields['institution_name'].widget.attrs['size'] = 60
@@ -24,8 +24,8 @@ class RegistrationSchoolChoiceForm(forms.Form):
     """
         A form for selecting an institution form institutionnames
     """
-    aashe_id = forms.IntegerField() 
-    
+    aashe_id = forms.IntegerField()
+
 #    class Meta:
 #        model = Institution
 #        fields = ['aashe_id']
@@ -39,7 +39,7 @@ class RegistrationForm(ModelForm):
         Contact Information Form
     """
     #terms_of_service = forms.BooleanField(label="I agree to the terms of service", required=True)
-    
+
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
         self.fields['contact_first_name'].label = "First Name"
@@ -55,17 +55,17 @@ class RegistrationForm(ModelForm):
         self.fields['executive_contact_title'].label = "Title"
         self.fields['executive_contact_department'].label = "Department/Office"
         self.fields['executive_contact_email'].label = "Email"
-        
+
         self.fields['executive_contact_first_name'].required = True
-        self.fields['executive_contact_middle_name'].required = True
+        self.fields['executive_contact_middle_name'].required = False
         self.fields['executive_contact_last_name'].required = True
         self.fields['executive_contact_title'].required = True
         self.fields['executive_contact_department'].required = True
         self.fields['executive_contact_email'].required = True
-    
+
     class Meta:
         model = Institution
-        fields =    [  
+        fields =    [
                         'contact_first_name',
                         'contact_middle_name',
                         'contact_last_name',
@@ -80,10 +80,10 @@ class RegistrationForm(ModelForm):
                         'executive_contact_department',
                         'executive_contact_email',
                     ]
-        
+
     def clean(self):
         cleaned_data = self.cleaned_data
-        
+
         # confirm that the contact email and the executive email are not the same
         if cleaned_data.has_key("contact_email") and cleaned_data.has_key("executive_contact_email"):
             contact = cleaned_data.get("contact_email")
@@ -102,7 +102,7 @@ class DataCollectorRegistrationForm(ModelForm):
     """
         Contact Information Form with just the liaison
     """
-    
+
     def __init__(self, *args, **kwargs):
         super(DataCollectorRegistrationForm, self).__init__(*args, **kwargs)
         self.fields['contact_first_name'].label = "First Name"
@@ -112,10 +112,10 @@ class DataCollectorRegistrationForm(ModelForm):
         self.fields['contact_department'].label = "Department/Office"
         self.fields['contact_phone'].label = "Phone"
         self.fields['contact_email'].label = "Email"
-    
+
     class Meta:
         model = Institution
-        fields =    [  
+        fields =    [
                         'contact_first_name',
                         'contact_middle_name',
                         'contact_last_name',
@@ -132,11 +132,11 @@ PARTICIPATION_CHOICES = (
 
 class ParticipationLevelForm(forms.Form):
     level = forms.fields.ChoiceField(widget=forms.widgets.RadioSelect, choices=PARTICIPATION_CHOICES)
-        
+
 class PayLaterForm(forms.Form):
-    
+
     confirm = forms.BooleanField(label="Please bill me and I will pay later.", required=False, widget=forms.CheckboxInput(attrs={'onchange': 'togglePayment(this);',}))
-        
+
 class PaymentForm(forms.Form):
     """
         Credit Card Payment form
@@ -152,49 +152,49 @@ class PaymentForm(forms.Form):
     billing_state = forms.CharField(max_length=2)
     billing_zipcode = forms.CharField(max_length=7, label='Billing ZIP code')
     discount_code = forms.CharField(max_length=16, required=False)
-    
+
     def clean_exp_month(self):
         data = self.cleaned_data['exp_month']
         error_text = "Please enter a number between 1 and 12"
-        
+
         if not self.is_numeric(data):
             raise forms.ValidationError(error_text)
         month = int(data)
         if month > 12 or month < 0:
             raise forms.ValidationError(error_text)
-            
+
         return data
-    
+
     def clean_discount_code(self):
         data = self.cleaned_data['discount_code']
         if data == "":
             return None
-        
+
         try:
             discount = ValueDiscount.objects.get_current().get(code=data)
         except ValueDiscount.DoesNotExist:
             raise forms.ValidationError("Invalid Discount Code")
-        
+
         return data
 
     def clean_exp_year(self):
         data = self.cleaned_data['exp_year']
         error_text = "Please enter a valid year"
-        
+
         if not self.is_numeric(data):
             raise forms.ValidationError(error_text)
 
         return data
-        
+
     def clean_billing_zipcode(self):
         data = self.cleaned_data['billing_zipcode']
         error_text = "Please enter a valid US or Canadian zip code"
-    
+
         if not is_usa_zipcode(data) and not is_canadian_zipcode(data):
             raise forms.ValidationError(error_text)
-    
+
         return data
-        
+
     def is_numeric(self, data):
         """ Helper function to indicate if data is numeric. """
         try:
@@ -202,13 +202,13 @@ class PaymentForm(forms.Form):
         except:
             return False
         return True
-    
+
 class RegistrationSurveyForm(ModelForm):
-    
+
     class Meta:
         model = RegistrationSurvey
         fields = ['source', 'reasons', 'other', 'primary_reason', 'enhancements']
-        
+
     def __init__(self, *args, **kwargs):
         from stars.apps.institutions.models import RegistrationReason
         super(RegistrationSurveyForm, self).__init__(*args, **kwargs)
@@ -222,21 +222,20 @@ class RegistrationSurveyForm(ModelForm):
         self.fields['primary_reason'].label = "Which of the above reasons, if any, was the primary reason your institution registered for STARS?"
 
 class RespondentRegistrationSurveyForm(ModelForm):
-    
+
     class Meta:
         model = RespondentSurvey
         fields = ['source', 'reasons', 'other', 'potential_stars']
-        
+
     def __init__(self, *args, **kwargs):
         super(RespondentRegistrationSurveyForm, self).__init__(*args, **kwargs)
-        
+
         from stars.apps.institutions.models import RespondentRegistrationReason
         choices = []
         for r in RespondentRegistrationReason.objects.all():
 #            if r.title != "Other" and r.title != "No reason was primary":
             choices.append((r.id, r.title))
-        
+
         self.fields['reasons'].widget = forms.CheckboxSelectMultiple(choices=choices)
         self.fields['reasons'].help_text = "Select all that apply"
         self.fields['reasons'].label = "The reason(s) your institution registered for the CSDC were to:"
-        
