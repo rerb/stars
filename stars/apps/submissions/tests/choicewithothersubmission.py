7296c0b67ffdf8640a94992adbdb909c3e0bea45
Bob Erb <bob.erb@aashe.org>
2012-08-15 15:59:27 -0400

    Replaced flashMessage in submission.models with django.contrib.messages.

--- stars/apps/submissions/tests/choicewithothersubmission.py
+++ stars/apps/submissions/tests/choicewithothersubmission.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 
 import testfixtures
 
+from stars.apps.credits.models import Choice
 from stars.apps.submissions.models import ChoiceWithOtherSubmission
 
 
@@ -20,3 +21,25 @@ class ChoiceWithOtherSubmissionTest(TestCase):
         self.assertEqual(log.records[0].levelname, 'ERROR')
         self.assertTrue(log.records[0].msg.startswith(
             'Attempt to decompress non-existing'))
+
+    def test_compress_warning_logging(self):
+        """Does compress log a warning message if data might be lost?
+        """
+        cwos = ChoiceWithOtherSubmission()
+        with testfixtures.LogCapture('stars') as log:
+            with testfixtures.Replacer() as r:
+                r.replace(
+                    'stars.apps.submissions.models.ChoiceWithOtherSubmission.'
+                    'get_last_choice',
+                    lambda x: MockChoice())
+                cwos.compress(choice='yellow', other_value=True)
+
+        self.assertEqual(len(log.records), 1)
+        self.assertEqual(log.records[0].levelname, 'WARNING')
+        self.assertTrue('will not be saved because' in log.records[0].msg)
+
+
+class MockChoice(object):
+
+    def __init__(self):
+        self.choice = 'blue'
