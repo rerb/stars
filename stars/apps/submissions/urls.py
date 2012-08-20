a10c8db11ebd4a0bc17d634f2056ce1c0e81827f
Bob Erb <bob.erb@aashe.org>
2012-08-20 15:48:18 -0400

    Rm'ed old commented-out line in submissions/urls.py.

--- stars/apps/submissions/urls.py
+++ stars/apps/submissions/urls.py
@@ -7,7 +7,6 @@ from stars.apps.submissions.api import CategoryPieChart, SubategoryPieChart, \
 v1_api = Api(api_name='v1')
 v1_api.register(CategoryPieChart())
 v1_api.register(SubategoryPieChart())
-#v1_api.register(SubmissionSetResource())
 v1_api.register(SummaryPieChart())
 
 urlpatterns = v1_api.urls
