e3fb4bd044376050f32dea7f0fa5a445bac7f6d9
jamstooks <jamstooks@localhost>
2012-08-21 00:54:25 -0400

    Added accuracy inquiry tests;

--- stars/apps/institutions/tests/__init__.py
+++ stars/apps/institutions/tests/__init__.py
@@ -1,12 +1,13 @@
 from stars.apps.institutions.api.test import InstitutionResourceTestCase
-from stars.apps.institutions.tests import stars_account, permissions, \
-     pending_accounts, test_tags
+from stars.apps.institutions.tests import stars_account, permissions, pending_accounts, test_tags
 from stars.apps.institutions.tests.institution import InstitutionTest
+from stars.apps.institutions.tests.accuracy_inquiry import AccuracyInquiryTest
 
 __test__ = {
     'permissions': permissions,
     'stars_account': stars_account,
     'pending_accounts': pending_accounts,
     'test_tags': test_tags,
-    'InstitutionTest': InstitutionTest
+    'InstitutionTest': InstitutionTest,
+    "AccuracyInquiryTest": AccuracyInquiryTest,
 }
