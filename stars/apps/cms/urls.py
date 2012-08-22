e4f9eb4951e4282db6c200b5a98c96276cb24bf7
Bob Erb <bob.erb@aashe.org>
2012-08-22 03:13:46 -0400

    Converted CMS views to generic class-based views.

--- stars/apps/cms/urls.py
+++ stars/apps/cms/urls.py
@@ -1,10 +1,21 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns
+
+from stars.apps.cms.views import ArticleDetailView, CategoryDetailView, \
+     OldPathRedirectView, SubcategoryDetailView
 
 urlpatterns = patterns('stars.apps.cms.views',
-    
-    (r'^(?P<category_slug>[^\/]+)/$', 'category_detail'),
-    (r'^(?P<category_slug>[^\/]+)/(?P<nid>\d+)/$', 'old_path'),
-    (r'^(?P<category_slug>[^\/]+)/(?P<article_slug>[^\/]+).html$', 'article_detail'),
-    (r'^(?P<category_slug>[^\/]+)/(?P<subcategory_slug>[^\/]+)/$', 'subcategory_detail'),
-    (r'^(?P<category_slug>[^\/]+)/(?P<subcategory_slug>[^\/]+)/(?P<article_slug>[^\/]+).html$', 'article_detail'),
+
+    (r'^(?P<category_slug>[^\/]+)/$', CategoryDetailView.as_view()),
+
+    (r'^[^\/]+/(?P<nid>\d+)/$', OldPathRedirectView.as_view()),
+
+    (r'^(?P<category_slug>[^\/]+)/(?P<article_slug>[^\/]+).html$',
+     ArticleDetailView.as_view()),
+
+    (r'^(?P<category_slug>[^\/]+)/(?P<subcategory_slug>[^\/]+)/$',
+     SubcategoryDetailView.as_view()),
+
+    (r'^(?P<category_slug>[^\/]+)/(?P<subcategory_slug>[^\/]+)/(?P<article_slug>[^\/]+).html$',
+     ArticleDetailView.as_view()),
+
 )
--- stars/apps/cms/views.py
+++ stars/apps/cms/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import get_object_or_404
 from django.http import Http404, HttpResponseRedirect
+from django.views.generic.base import RedirectView, TemplateView
 
-from stars.apps.cms.models import *
-from stars.apps.helpers.views import TemplateView
+from stars.apps.cms.models import Category, NewArticle, Subcategory
 
 
 class CMSView(TemplateView):
-    """ A base context for all CMS views """
-
-    @property
-    def __name__(self):
-        return self.__class__.__name__
-
-    def get_context(self, request, *args, **kwargs):
+    """
+        A base context for all CMS views
+    """
+    def get_context_data(self, *args, **kwargs):
         """ Add/update any context variables """
 
         # Creating inital values for each of these so that
@@ -21,10 +18,12 @@ class CMSView(TemplateView):
 
         if kwargs.has_key('category_slug'):
             try:
-                category = Category.objects.get(slug=kwargs['category_slug'], published=True)
+                category = Category.objects.get(slug=kwargs['category_slug'],
+                                                published=True)
             except Category.DoesNotExist:
                 try:
-                    subcategory = Subcategory.objects.get(slug=kwargs['category_slug'], published=True)
+                    subcategory = Subcategory.objects.get(
+                        slug=kwargs['category_slug'], published=True)
                     return HttpResponseRedirect(subcategory.get_absolute_url())
                 except Subcategory.DoesNotExist:
                     raise Http404
@@ -32,26 +31,50 @@ class CMSView(TemplateView):
             _context['category'] = category
 
             if kwargs.has_key('subcategory_slug'):
-                subcategory = get_object_or_404(Subcategory, slug=kwargs['subcategory_slug'], parent=category, published=True)
+                subcategory = get_object_or_404(Subcategory,
+                                                slug=kwargs['subcategory_slug'],
+                                                parent=category, published=True)
                 _context['subcategory'] = subcategory
             else:
                 subcategory = None
 
             if kwargs.has_key('article_slug'):
-                article = get_object_or_404(NewArticle, slug=kwargs['article_slug'], published=True)
-                if category not in article.categories.all() and subcategory not in article.subcategories.all():
+                article = get_object_or_404(NewArticle,
+                                            slug=kwargs['article_slug'],
+                                            published=True)
+                if (category not in article.categories.all() and
+                    subcategory not in article.subcategories.all()):
                     raise Http404
                 _context['article'] = article
 
         return _context
 
-category_detail = CMSView(template='cms/category_detail.html')
-subcategory_detail = CMSView(template='cms/subcategory_detail.html')
-article_detail = CMSView(template='cms/article_detail.html')
 
-def old_path(request, category_slug, nid):
+class ArticleDetailView(CMSView):
     """
-        Forwards from the old link system.
+        Detail page for one CMS article.
     """
-    article = get_object_or_404(NewArticle, irc_id=nid, published=True)
-    return HttpResponseRedirect(article.get_absolute_url())
+    template_name = 'cms/article_detail.html'
+
+
+class CategoryDetailView(CMSView):
+    """
+        Detail page for a CMS category.
+    """
+    template_name = 'cms/category_detail.html'
+
+
+class SubcategoryDetailView(CMSView):
+    """
+        Detail page for a CMS subcategory.
+    """
+    template_name = 'cms/subcategory_detail.html'
+
+
+class OldPathRedirectView(RedirectView):
+    """
+        Redirects article URLs from the old link system.
+    """
+    def get_redirect_url(self, nid):
+        article = get_object_or_404(NewArticle, irc_id=nid, published=True)
+        return article.get_absolute_url()
