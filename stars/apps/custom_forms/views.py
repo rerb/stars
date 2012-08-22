025947a6b917ee0c0c212af2d6d7377203b7a072
Bob Erb <bob.erb@aashe.org>
2012-08-22 13:37:07 -0400

    Rm'ed unused imports form apps/custom_forms/views.py.

--- stars/apps/custom_forms/views.py
+++ stars/apps/custom_forms/views.py
@@ -1,21 +1,20 @@
 from stars.apps.custom_forms.forms import *
-from stars.apps.helpers.forms.views import FormActionView, MultiFormView
+from stars.apps.helpers.forms.views import FormActionView
 
 from django.views.generic.simple import direct_to_template
 from django.core.mail import send_mail
 from django.template.loader import get_template
 from django.conf import settings
-from django.template import Context, Template
+from django.template import Context
 from stars.apps.notifications.models import EmailTemplate
 
-import sys
 
 class EligibilityView(FormActionView):
-    
+
     def get_success_action(self, request, context, form):
-        
+
         self.save_form(form, request, context)
-        
+
         responses = []
         for field in form:
             value = field.data
@@ -25,11 +24,11 @@ class EligibilityView(FormActionView):
                 else:
                     value = "NO"
             responses.append({'label': field.label, 'value': value})
-        
+
         _context = Context({'responses': responses,})
         t = get_template("custom_forms/eligibility_staff_email.txt")
         message = t.render(_context)
-        
+
         email_to = ['stars@aashe.org',]
         send_mail(  "Eligibility Query from %s at %s" % (form.cleaned_data['name'], form.cleaned_data['institution']),
                     message,
@@ -37,8 +36,8 @@ class EligibilityView(FormActionView):
                     email_to,
                     fail_silently=False
                     )
-        
-        
+
+
         t = get_template("custom_forms/eligibility_query_response.txt")
         message = t.render(_context)
         send_mail( "Thank you for your enquiry",
@@ -47,35 +46,35 @@ class EligibilityView(FormActionView):
                    [form.cleaned_data['email'],],
                    fail_silently=False
                    )
-        
+
         r = direct_to_template(request, "custom_forms/form_success.html", extra_context=self.context_dict)
         return r
-    
+
 eligibility = EligibilityView("custom_forms/eligibility.html", EligibilityForm, has_upload=False, form_name='object_form', init_context={'form_title': "Eligibility Inquiry",})
 
 class TAAppView(FormActionView):
-    
+
     def get_success_action(self, request, context, form):
-        
+
         self.save_form(form, request, context)
 
         et = EmailTemplate.objects.get(slug="ta_application")
         et.send_email([request.POST['email']], context)
-        
+
         r = direct_to_template(request, "custom_forms/form_success.html", extra_context=self.context_dict)
         return r
 
 ta_application = TAAppView("custom_forms/ta_app.html", TAApplicationForm, has_upload=True, form_name='object_form', init_context={'form_title': "Technical Advisor Application",})
 
 class SteeringCommitteeNominationView(FormActionView):
-    
+
     def get_success_action(self, request, context, form):
-        
+
         self.save_form(form, request, context)
-        
+
         et = EmailTemplate.objects.get(slug="sc_application")
         et.send_email([request.POST['email']], context)
-        
+
         r = direct_to_template(request, "custom_forms/form_success.html", extra_context=self.context_dict)
         return r
 
