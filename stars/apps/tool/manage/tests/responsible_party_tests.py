e5f7d235b45a3143645b8834b9a4366a4e836970
Bob Erb <bob.erb@aashe.org>
2012-09-13 13:33:18 -0400

    Added inst slugs to urls in responsible_party_tests.py. They still don't pass, though.  Looks like StarsAccount.has_perm() isn't getting called, so instead of 200's, the tests get 403's.

--- stars/apps/tool/manage/tests/responsible_party_tests.py
+++ stars/apps/tool/manage/tests/responsible_party_tests.py
@@ -3,11 +3,7 @@
 """
 from django.test import TestCase
 from django.test.client import Client
-from django.conf import settings
 from django.contrib.auth.models import User
-
-from datetime import date
-import sys, os, random
 
 from stars.apps.submissions.models import ResponsibleParty
 
@@ -16,46 +12,66 @@ class ResponisblePartyTest(TestCase):
 
     def setUp(self):
         pass
-    
+
     def test_delete(self):
         """
-        
+
         """
         print self.fixtures
         user = User.objects.get(pk=1)
         user.set_password('test')
         user.save()
-        
+
         c = Client()
         login = c.login(username='tester', password='test')
         self.assertTrue(login)
-        
-        rp_url = "/tool/manage/responsible-parties/1/"
-        rp_del_url = "/tool/manage/responsible-parties/1/delete/"
-        
+
+        rp = ResponsibleParty.objects.get(pk=1)
+
+        rp_url = "/tool/{slug}/manage/responsible-parties/{id}/".format(
+            slug=rp.institution.slug, id=rp.id)
+
+        rp_del_url = \
+          "/tool/{slug}/manage/responsible-parties/{id}/delete/".format(
+              slug=rp.institution.slug, id=rp.id)
+
         # RP Page Loads... and RP exists
         response = c.get(rp_url)
-        self.assertTrue(response.status_code == 200)
 
-        # Delete should fail and redirect to RP 
+        self.assertEqual(response.status_code, 200)
+
+        # Delete should fail and redirect to RP
         response = c.get(rp_del_url)
-        self.assertTrue(response.status_code == 302)
-        self.assertEqual(response['Location'], "http://testserver/tool/manage/responsible-parties/1/")
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response['Location'],
+            "http://testserver/tool/{slug}/manage/"
+            "responsible-parties/{id}/".format(
+                slug=rp.institution.slug, id=rp.id))
 
         # Confirm RP1 still exists
         self.assertEqual(ResponsibleParty.objects.count(), 2)
-        
-        rp_url = "/tool/manage/responsible-parties/2/"
-        rp_del_url = "/tool/manage/responsible-parties/2/delete/"
+
+        rp = ResponsibleParty.objects.get(pk=2)
+
+        rp_url = "/tool/{slug}/manage/responsible-parties/{id}/".format(
+            slug=rp.institution.slug, id=rp.id)
+
+        rp_del_url = \
+          "/tool/{slug}/manage/responsible-parties/{id}/delete/".format(
+              slug=rp.institution.slug, id=rp.id)
 
         # RP Page Loads... and RP exists
         response = c.get(rp_url)
-        self.assertTrue(response.status_code == 200)
+        self.assertEqual(response.status_code, 200)
 
-        # Delete should succeed and redirect to RP list 
+        # Delete should succeed and redirect to RP list
         response = c.get(rp_del_url)
-        self.assertTrue(response.status_code == 302)
-        self.assertEqual(response['Location'], "http://testserver/tool/manage/responsible-parties/")
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response['Location'],
+            "http://testserver/tool/{slug}/manage/responsible-parties/".format(
+                slug=rp.institution.slug))
 
         # Confirm RP2 still exists
         self.assertEqual(ResponsibleParty.objects.count(), 1)
