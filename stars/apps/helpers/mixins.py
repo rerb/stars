8cb5d99bbcf8584b356c4495972cf4c040ccc0ef
Bob Erb <bob.erb@aashe.org>
2012-09-17 16:02:01 -0400

    Modified added valid_message and invalid_message attrs to StarsFormMixin. Allows descendents to specify the messages get_valid_message() and get_invalid_messages() return.

--- stars/apps/helpers/mixins.py
+++ stars/apps/helpers/mixins.py
@@ -4,16 +4,19 @@ class StarsFormMixin(object):
     """
         A custom mixin to add a message upon success
     """
+    valid_message = "Changes Saved Successfully"
+    invalid_message = "Please correct the errors below"
+
     def get_valid_message(self):
-        return "Changes Saved Successfully"
-    
+        return self.valid_message
+
     def get_invalid_message(self):
-        return "Please correct the errors below"
-    
+        return self.invalid_message
+
     def form_valid(self, form):
         messages.success(self.request, self.get_valid_message())
         return super(StarsFormMixin, self).form_valid(form)
-    
+
     def form_invalid(self, form):
         messages.error(self.request, self.get_invalid_message())
         return super(StarsFormMixin, self).form_invalid(form)
