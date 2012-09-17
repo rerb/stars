20ae72aa3e9ba2e1d7e185ad73457e76f937ea35
Bob Erb <bob.erb@aashe.org>
2012-09-17 16:12:22 -0400

    Added tests for new responsible party generic class-based views.

--- stars/apps/tool/manage/tests/__init__.py
+++ stars/apps/tool/manage/tests/__init__.py
@@ -1,14 +1,16 @@
 from renewal_tests import RenewalTest
-from responsible_party_tests import ResponisblePartyTest
 from views import ViewsTest, InstitutionPaymentsViewTest, \
-     ResponsiblePartyListViewTest
-from views import ResponsiblePartyEditViewTest
+     ResponsiblePartyCreateViewTest, ResponsiblePartyDeleteViewTest, \
+     ResponsiblePartyListViewTest, ResponsiblePartyEditViewTest, \
+     TopLevelFunctionsTest
 
 __test__ = {
     'renewal_test': RenewalTest,
-    'responsible_party_test': ResponisblePartyTest,
     'views': ViewsTest,
     'intitution_payments_view_test': InstitutionPaymentsViewTest,
     'responsible_party_view_test': ResponsiblePartyListViewTest,
-    'responsible_party_edit_view_test': ResponsiblePartyEditViewTest
+    'responsible_party_edit_view_test': ResponsiblePartyEditViewTest,
+    'responsible_party_create_view_test': ResponsiblePartyCreateViewTest,
+    'responsible_party_delete_view_test': ResponsiblePartyDeleteViewTest,
+    'top_level_functions_test': TopLevelFunctionsTest,
 }
