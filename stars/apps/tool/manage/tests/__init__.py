7d85e267da9f2b9a6bacf851df73a7fb3175ecc0
Bob Erb <bob.erb@aashe.org>
2012-09-26 11:59:06 -0400

    Simplified tool.manage.tests.__init__.py. Don't need __tests__ defined since the tests are not doctests (right?). Don't need to import each test by name, if we don't need to name them in __tests__, either.

--- stars/apps/tool/manage/tests/__init__.py
+++ stars/apps/tool/manage/tests/__init__.py
@@ -1,14 +1,2 @@
-from renewal_tests import RenewalTest
-from views import ViewsTest, InstitutionPaymentsViewTest, \
-     ResponsiblePartyCreateViewTest, ResponsiblePartyDeleteViewTest, \
-     ResponsiblePartyListViewTest, ResponsiblePartyEditViewTest
-
-__test__ = {
-    'renewal_test': RenewalTest,
-    'views': ViewsTest,
-    'intitution_payments_view_test': InstitutionPaymentsViewTest,
-    'responsible_party_view_test': ResponsiblePartyListViewTest,
-    'responsible_party_edit_view_test': ResponsiblePartyEditViewTest,
-    'responsible_party_create_view_test': ResponsiblePartyCreateViewTest,
-    'responsible_party_delete_view_test': ResponsiblePartyDeleteViewTest
-}
+from renewal_tests import *
+from views import *
