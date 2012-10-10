552202aab21de7eda820f2e099ced104cb1f2530
Bob Erb <bob.erb@aashe.org>
2012-10-10 14:07:10 -0400

    Named 'tool-dashboard' URL.

--- stars/apps/tool/urls.py
+++ stars/apps/tool/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import include, patterns, url
 
 urlpatterns = patterns('',
-    (r'^$', 'stars.apps.tool.views.tool_dashboard'),
+    url(r'^$', 'stars.apps.tool.views.tool_dashboard',
+        name='tool-dashboard'),
     (r'^credit-editor/', include('stars.apps.tool.credit_editor.urls')),
     (r'^admin/', include('stars.apps.tool.admin.urls')),
     (r'^submissions/', include('stars.apps.tool.my_submission.urls')),
     (r'^my-resources/', include('stars.apps.tool.my_resources.urls')),
-    (r'^(?P<institution_slug>[^/]*)/manage/', include('stars.apps.tool.manage.urls')),
+    (r'^(?P<institution_slug>[^/]*)/manage/',
+     include('stars.apps.tool.manage.urls')),
 )
