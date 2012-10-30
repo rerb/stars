799d1c1a6c82870a0cda7b21ee29885921882dc6
Bob Erb <bob.erb@aashe.org>
2012-10-30 18:39:24 -0400

    Tests for apps.institution.models.Subscription handle recent changes there.

--- stars/apps/institutions/tests/subscription.py
+++ stars/apps/institutions/tests/subscription.py
@@ -1,119 +1,148 @@
 """Tests for apps.institutions.models.Subscription.
 """
 from datetime import date, timedelta
-from unittest import TestCase
+import sys
+if sys.version < '2.7':
+    from django.utils.unittest import TestCase
+else:
+    from unittest import TestCase
 
 import testfixtures
 
 from stars.apps.institutions.models import Subscription
-from stars.apps.registration.models import ValueDiscount
+from stars.apps.registration.models import DiscountManager, ValueDiscount
 from stars.test_factories import InstitutionFactory, SubscriptionFactory
 
 
 class SubscriptionTest(TestCase):
 
-    def test__get_reason_for_new_subscription_member_initial(self):
-        """Does _get_reason_for_new_subscription work for an initial member sub?
-        """
-        self.assertEqual(
-            Subscription._get_reason_for_new_subscription(
-                InstitutionFactory(is_member=True)),
-            'member_registration')
+    def setUp(self):
+        self.subscription = SubscriptionFactory(
+            institution=InstitutionFactory())
+        self.member = InstitutionFactory(is_member=True)
+        self.nonmember = InstitutionFactory(is_member=False)
 
-    def test__get_reason_for_new_subscription_nonmember_initial(self):
-        """Does _get_reason_for_new_subscription work for initial nonmember sub?
-        """
-        self.assertEqual(
-            Subscription._get_reason_for_new_subscription(InstitutionFactory()),
-            'nonmember_registration')
+    def test__apply_standard_discount(self):
+        price = 10000
+        self.assertLess(self.subscription._apply_standard_discount(price),
+                        price)
 
-    def test__get_reason_for_new_subscription_member_renewal(self):
-        """Does _get_reason_for_new_subscription work for a member renewal?
-        """
-        subscription = SubscriptionFactory(
-            institution=InstitutionFactory(is_member=True))
-        self.assertEqual(
-            Subscription._get_reason_for_new_subscription(
-                subscription.institution),
-            'member_renewal')
+    #################################
+    # _calculate_reason() tests:
+    #################################
 
-    def test__get_reason_for_new_subscription_nonmember_renewal(self):
-        """Does _get_reason_for_new_subscription work for a nonmember renewal?
+    def test__calculate_reason_member_initial(self):
+        """Does _calculate_reason work for an initial member subscription?
         """
-        subscription = SubscriptionFactory()
-        self.assertEqual(Subscription._get_reason_for_new_subscription(
-            subscription.institution),
-            'nonmember_renewal')
+        self.subscription.institution = self.member
+        self.assertEqual(self.subscription._calculate_reason(),
+                         'member_registration')
 
-    def test__get_start_date_for_new_subscription_lapsed_subscription(self):
-        """Does _get_start_date_for_new_subscription work when sub is lapsed?
+    def test__calculate_reason_nonmember_initial(self):
+        """Does _calculate_reason work for initial nonmember subscription?
+        """
+        self.subscription.institution = self.nonmember
+        self.assertEqual(self.subscription._calculate_reason(),
+                         'nonmember_registration')
+
+    def test__calculate_reason_member_renewal(self):
+        """Does _calculate_reason work for a member renewal?
+        """
+        self.subscription.institution = self.member
+        self.subscription.save()
+        new_subscription = SubscriptionFactory(institution=self.member)
+        self.assertEqual(new_subscription._calculate_reason(),
+                         'member_renewal')
+
+    def test__calculate_reason_nonmember_renewal(self):
+        """Does _calculate_reason work for a nonmember renewal?
+        """
+        self.subscription.institution = self.nonmember
+        self.subscription.save()
+        new_subscription = SubscriptionFactory(institution=self.nonmember)
+        self.assertEqual(new_subscription._calculate_reason(),
+                         'nonmember_renewal')
+
+    ################################
+    # calculate_start_date() tests:
+    ################################
+
+    def test__calculate_start_date_lapsed_subscription(self):
+        """Does _calculate_start_date work when subscription is lapsed?
         """
         with testfixtures.Replacer() as r:
-            r.replace('stars.apps.institutions.models.Institution.'
-                      'get_last_subscription_end',
+            r.replace('stars.apps.institutions.models.Subscription.'
+                      '_get_latest_subscription_end',
                       lambda x : date.today() - timedelta(days=200))
-            start_date = Subscription._get_start_date_for_new_subscription(
-                InstitutionFactory())
+            start_date = self.subscription._calculate_start_date()
         self.assertEqual(start_date, date.today())
 
-    def test__get_start_date_for_new_subscription_current_subscription(self):
-        """Does _get_start_date_for_new_subscription work when sub is current?
+    def test__calculate_start_date_current_subscription(self):
+        """Does _calculate_start_date work when subscription is current?
         """
         DAYS_UNTIL_SUB_EXPIRES = 200
         with testfixtures.Replacer() as r:
             r.replace(
-                'stars.apps.institutions.models.Institution.'
-                'get_last_subscription_end',
+                'stars.apps.institutions.models.Subscription.'
+                '_get_latest_subscription_end',
                 lambda x : date.today() + timedelta(days=DAYS_UNTIL_SUB_EXPIRES))
-            start_date = Subscription._get_start_date_for_new_subscription(
-                InstitutionFactory())
+            start_date = self.subscription._calculate_start_date()
         self.assertEqual(
             start_date,
             date.today() + timedelta(days=DAYS_UNTIL_SUB_EXPIRES + 1))
 
-    def test__get_start_date_for_new_subscription_subscription_ends_today(self):
-        """Does _get_start_date_for_new_subscription work when sub ends today?
+    def test__calculate_start_date_subscription_ends_today(self):
+        """Does _calculate_start_date work when subscription ends today?
         """
         with testfixtures.Replacer() as r:
-            r.replace('stars.apps.institutions.models.Institution.'
-                      'get_last_subscription_end',
+            r.replace('stars.apps.institutions.models.Subscription.'
+                      '_get_latest_subscription_end',
                       lambda x : date.today())
-            start_date = Subscription._get_start_date_for_new_subscription(
-                InstitutionFactory())
+            start_date = self.subscription._calculate_start_date()
         self.assertEqual(start_date, date.today() + timedelta(1))
+
+    ####################################
+    # _subscription_discounted() tests:
+    ####################################
 
     def test__subscription_discounted_no_previous_subscription(self):
         """Does _subscription_discounted work when there's no prev subscription?
         """
-        self.assertFalse(
-            Subscription._subscription_discounted(InstitutionFactory()))
+        self.assertFalse(self.subscription._subscription_discounted())
 
     def test__subscription_discounted_current_subscription(self):
         """Does _subscription_discounted work when a subscription is current?
         """
-        subscription = SubscriptionFactory(
-            start_date=date.today() - timedelta(days=5),
-            end_date=date.today() + timedelta(days=5))
-        self.assertTrue(
-            Subscription._subscription_discounted(subscription.institution))
+        current_start_date = date.today() - timedelta(days=5)
+        current_end_date = date.today() + timedelta(days=5)
+        SubscriptionFactory(institution=self.subscription.institution,
+                            start_date=current_start_date,
+                            end_date=current_end_date)
+        self.assertTrue(self.subscription._subscription_discounted())
 
     def test__subscription_discounted_recently_lapsed_subscription(self):
         """Does _subscription_discounted work when subs is recently lapsed?
         """
-        subscription = SubscriptionFactory(
-            start_date=date.today() - timedelta(days=100),
-            end_date=date.today() - timedelta(days=5))
-        self.assertTrue(
-            Subscription._subscription_discounted(subscription.institution))
+        lapsed_start_date = date.today() - timedelta(days=100)
+        lapsed_end_date = date.today() - timedelta(days=5)
+        SubscriptionFactory(institution=self.subscription.institution,
+                            start_date=lapsed_start_date,
+                            end_date=lapsed_end_date)
+        self.assertTrue(self.subscription._subscription_discounted())
 
     def test__subscription_discounted_long_time_lapsed_subscription(self):
         """Does _subscription_discounted work when a subs lapsed long time ago?
         """
-        subscription = SubscriptionFactory(
-            start_date=date.today() - timedelta(days=1000),
-            end_date=date.today() - timedelta(days=900))
-        self.assertFalse(
-            Subscription._subscription_discounted(subscription.institution))
+        lapsed_start_date = date.today() - timedelta(days=1000)
+        lapsed_end_date = date.today() - timedelta(days=900)
+        SubscriptionFactory(institution=self.subscription.institution,
+                            start_date=lapsed_start_date,
+                            end_date=lapsed_end_date)
+        self.assertFalse(self.subscription._subscription_discounted())
+
+    #############################
+    # _apply_promo_code() tests:
+    #############################
 
     def test__apply_promo_code_valid_promo_code(self):
         """Does _apply_promo_code work for a valid promo code?
@@ -127,8 +156,9 @@ class SubscriptionTest(TestCase):
             end_date=date.today() + timedelta(days=1))
         value_discount.save()
         initial_price = 5432
-        self.assertEqual(Subscription._apply_promo_code(initial_price,
-                                                        promo_code),
+        self.assertEqual(
+            self.subscription._apply_promo_code(initial_price,
+                                                promo_code),
             initial_price - promo_amount)
 
     def test__apply_promo_code_expired_promo_code(self):
@@ -143,30 +173,33 @@ class SubscriptionTest(TestCase):
             end_date=date.today() - timedelta(days=10))
         value_discount.save()
         initial_price = 1000
-        self.assertEqual(Subscription._apply_promo_code(initial_price,
-                                                        promo_code),
-            initial_price)
+        self.assertEqual(self.subscription._apply_promo_code(initial_price,
+                                                             promo_code),
+                         initial_price)
 
     def test__apply_promo_code_invalid_promo_code(self):
         """Does _apply_promo_code work for an invalid promo code?
         (Where 'invalid' means nonexistant.)
         """
         initial_price = 10
-        self.assertEqual(Subscription._apply_promo_code(initial_price,
-                                                        'BO-O-O-GUS CODE'),
+        self.assertEqual(self.subscription._apply_promo_code(initial_price,
+                                                             'BO-O-O-GUS CODE'),
                          initial_price)
 
     def test__apply_promo_code_no_promo_code(self):
         """Does _apply_promo_code work when no promo code is supplied?
         """
         initial_price = 10
-        self.assertEqual(Subscription._apply_promo_code(initial_price),
+        self.assertEqual(self.subscription._apply_promo_code(initial_price),
                          initial_price)
 
+    ###########################
+    # calculate_price() tests:
+    ###########################
 
-    def _test__get_price_for_new_subscription(self, is_member, use_promo,
-                                              gets_discount):
-        """Tests _get_price_for_new_subscription.
+    def _test_calculate_price(self, is_member, use_promo,
+                              gets_discount):
+        """Tests calculate_price.
 
         is_member, use_promo, and gets_discount are booleans that map
         to the three qualities that influence a subscription's price.
@@ -184,25 +217,27 @@ class SubscriptionTest(TestCase):
 
         Individual tests below exercise these combinations.
         """
+        self.subscription.institution = (self.member if is_member
+                                         else self.nonmember)
+        self.subscription.save()
+
         if use_promo:
             promo_code = 'WHO SAID THAT?'
             promo_amount = 50
-            value_discount = ValueDiscount(
+            promo_discount = ValueDiscount(
                 code=promo_code,
                 amount=promo_amount,
                 start_date=date.today() - timedelta(days=1),
                 end_date=date.today() + timedelta(days=1))
-            value_discount.save()
+            promo_discount.save()
         else:
             promo_code = None
 
-        with testfixtures.Replacer() as r:
-            r.replace('stars.apps.institutions.models.Subscription.'
-                      '_subscription_discounted',
-                      lambda *args : gets_discount)
-            price, discounted = Subscription._get_price_for_new_subscription(
-                InstitutionFactory(is_member=is_member),
-                                   promo_code=promo_code)
+        self.subscription._subscription_discounted = (
+            lambda *args : gets_discount)
+
+        price, promo_discount_applied = self.subscription.calculate_price(
+            promo_code=promo_code)
 
         price_check = (Subscription.MEMBER_BASE_PRICE if is_member else
                        Subscription.NONMEMBER_BASE_PRICE)
@@ -211,59 +246,60 @@ class SubscriptionTest(TestCase):
             price_check -= promo_amount
 
         if gets_discount:
-            price_check /= 2
+            price_check = self.subscription._apply_standard_discount(
+                price_check)
 
         self.assertEqual(price, price_check)
-        if gets_discount:
-            self.assertTrue(discounted)
-        else:
-            self.assertFalse(discounted)
 
-    def test__get_price_for_new_subscription_member_promo_discount(self):
-        """Does _get_price... work for member with promo and discount?
+        if DiscountManager().is_code_current(promo_code):
+            self.assertTrue(promo_discount_applied)
+        else:
+            self.assertFalse(promo_discount_applied)
+
+    def test_calculate_price_member_promo_discount(self):
+        """Does calculate_price work for member with promo and discount?
         """
-        self._test__get_price_for_new_subscription(
+        self._test_calculate_price(
             is_member=True, use_promo=True, gets_discount=True)
 
-    def test__get_price_for_new_subscription_nonmember_promo_discount(self):
-        """Does _get_price... work for nonmember with promo and discount?
+    def test_calculate_price_nonmember_promo_discount(self):
+        """Does calculate_price work for nonmember with promo and discount?
         """
-        self._test__get_price_for_new_subscription(
+        self._test_calculate_price(
             is_member=False, use_promo=True, gets_discount=True)
 
-    def test__get_price_for_new_subscription_member_no_promo_discount(self):
-        """Does _get_price... work for member with no promo and discount?
+    def test_calculate_price_member_no_promo_discount(self):
+        """Does calculate_price work for member with no promo and discount?
         """
-        self._test__get_price_for_new_subscription(
+        self._test_calculate_price(
             is_member=True, use_promo=False, gets_discount=True)
 
-    def test__get_price_for_new_subscription_nonmember_no_promo_discount(self):
-        """Does _get_price... work for nonmember with no promo and discount?
+    def test_calculate_price_nonmember_no_promo_discount(self):
+        """Does calculate_price work for nonmember with no promo and discount?
         """
-        self._test__get_price_for_new_subscription(
+        self._test_calculate_price(
             is_member=False, use_promo=False, gets_discount=True)
 
-    def test__get_price_for_new_subscription_member_no_promo_no_discount(self):
-        """Does _get_price... work for member with no promo and no discount?
+    def test_calculate_price_member_no_promo_no_discount(self):
+        """Does calculate_price work for member with no promo and no discount?
         """
-        self._test__get_price_for_new_subscription(
+        self._test_calculate_price(
             is_member=True, use_promo=False, gets_discount=False)
 
-    def test__get_price_for_new_subscription_nonmember_no_promo_no_discount(
-            self):
-        """Does _get_price... work for nonmember with no promo and no discount?
+    def test_calculate_price_nonmember_no_promo_no_discount(self):
+        """Does calculate_price work for nonmember with no promo and no discount?
         """
-        self._test__get_price_for_new_subscription(
+        self._test_calculate_price(
             is_member=False, use_promo=False, gets_discount=False)
 
-    def test__get_price_for_new_subscription_member_promo_no_discount(self):
-        """Does _get_price... work for member with promo and no discount?
+    def test_calculate_price_member_promo_no_discount(self):
+        """Does calculate_price work for member with promo and no discount?
         """
-        self._test__get_price_for_new_subscription(
+        self._test_calculate_price(
             is_member=True, use_promo=True, gets_discount=False)
 
-    def test__get_price_for_new_subscription_nonmember_promo_no_discount(self):
-        """Does _get_price... work for nonmember with promo and no discount?
+    def test_calculate_price_nonmember_promo_no_discount(self):
+        """Does calculate_price work for nonmember with promo and no discount?
         """
-        self._test__get_price_for_new_subscription(
+        self._test_calculate_price(
             is_member=False, use_promo=True, gets_discount=False)
