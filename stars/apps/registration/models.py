be3ee8fed8d8974bf1c7049867f05c5f2a428016
Bob Erb <bob.erb@aashe.org>
2012-10-30 16:58:43 -0400

    Added is_code_current method to apps.registration.models.DiscountManager.

--- stars/apps/registration/models.py
+++ stars/apps/registration/models.py
@@ -3,10 +3,18 @@ from datetime import date
 from django.db import models
 
 class DiscountManager(models.Manager):
-    
+
     def get_current(self):
         """ Only current discounts """
-        return ValueDiscount.objects.filter(start_date__lte=date.today()).filter(end_date__gte=date.today())
+        return ValueDiscount.objects.filter(
+            start_date__lte=date.today()).filter(end_date__gte=date.today())
+
+    def is_code_current(self, code):
+        """
+           Checks if `code` matches a current discount.
+        """
+        return code in [ value_discount.code for value_discount
+                         in self.get_current() ]
 
 class ValueDiscount(models.Model):
     objects = DiscountManager()
