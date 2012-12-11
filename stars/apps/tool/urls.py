18afae922352c9d16932e7f80df153747203c56c
Bob Erb <bob.erb@aashe.org>
2012-12-11 12:36:41 -0500

    Added named urls tool-landing-page, select-institution, and no-stars-account. To stars/apps/tool/urls.py.

--- stars/apps/tool/urls.py
+++ stars/apps/tool/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls.defaults import include, patterns, url
 
-from stars.apps.tool.views import SummaryToolView
+from stars.apps.tool.views import (NoStarsAccountView, SelectInstitutionView,
+                                   SummaryToolView, ToolLandingPageView)
 
-urlpatterns = patterns('',
+urlpatterns = patterns("",
+    url('^$', ToolLandingPageView.as_view(), name='tool-landing-page'),
+    url('^no-stars-account/', NoStarsAccountView.as_view(),
+        name='no-stars-account'),
+    url('^select-institution/', SelectInstitutionView.as_view(),
+        name='select-institution'),
     (r'^credit-editor/', include('stars.apps.tool.credit_editor.urls')),
     (r'^admin/', include('stars.apps.tool.admin.urls')),
     (r'^submissions/', include('stars.apps.tool.my_submission.urls')),
