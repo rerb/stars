9ce0eef1df2e5f5d503da77a253ab009df36ddc9
Bob Erb <bob.erb@aashe.org>
2012-12-17 21:37:24 -0500

    Added submissionset parameter to my_submissions URLs.

--- stars/apps/tool/urls.py
+++ stars/apps/tool/urls.py
@@ -11,10 +11,14 @@ urlpatterns = patterns("",
         name='select-institution'),
     (r'^credit-editor/', include('stars.apps.tool.credit_editor.urls')),
     (r'^admin/', include('stars.apps.tool.admin.urls')),
-    (r'^submissions/', include('stars.apps.tool.my_submission.urls')),
+
+    (r'^(?P<institution_slug>[^/]*)/submission/(?P<submissionset>\d+)/',
+     include('stars.apps.tool.my_submission.urls')),
+
     (r'^my-resources/', include('stars.apps.tool.my_resources.urls')),
     url(r'^(?P<institution_slug>[^/]*)/$', SummaryToolView.as_view(),
         name='tool-summary'),
+
     (r'^(?P<institution_slug>[^/]*)/manage/',
      include('stars.apps.tool.manage.urls')),
 )
