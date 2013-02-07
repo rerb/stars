78cbbbe37d17ff973a2f21cc9f7424e17e8174a8
jamstooks <jamstooks@localhost>
2013-02-07 13:20:03 -0700

    Updated ETL to add a "deleted_date" field to all ETL objects. This allows Sam to know when they have been deleted;

--- stars/apps/etl_export/utils.py
+++ stars/apps/etl_export/utils.py
@@ -1,17 +1,17 @@
-from stars.apps.etl_export.models import Institution, SubmissionSet, Subscription, SubscriptionPayment, Boundary
-        
+from stars.apps.etl_export.models import (Institution, SubmissionSet,
+        Subscription, SubscriptionPayment, Boundary)
 import sys
 
+
 def update_etl():
-    
-    print >> sys.stdout, "Exporting SubmissionSets" 
+
+    print >> sys.stdout, "Exporting SubmissionSets"
     SubmissionSet.etl_run_update()
-    print >> sys.stdout, "Exporting Institutions" 
+    print >> sys.stdout, "Exporting Institutions"
     Institution.etl_run_update()
-    print >> sys.stdout, "Exporting Subscriptions" 
+    print >> sys.stdout, "Exporting Subscriptions"
     Subscription.etl_run_update()
-    print >> sys.stdout, "Exporting Subscription payments" 
+    print >> sys.stdout, "Exporting Subscription payments"
     SubscriptionPayment.etl_run_update()
-    print >> sys.stdout, "Exporting Boundaries" 
+    print >> sys.stdout, "Exporting Boundaries"
     Boundary.etl_run_update()
-    
