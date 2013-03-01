af4ed6390da41286ff19b4bf40751e3a701bd3ae
jamstooks <jamstooks@localhost>
2013-03-01 15:37:01 -0700

    A littel pep8 update for this to clear up my editor;

--- stars/apps/submissions/admin.py
+++ stars/apps/submissions/admin.py
@@ -18,47 +18,59 @@ class BoundaryAdmin(admin.ModelAdmin):
     search_fields = ('submissionset__institution__name',)
 admin.site.register(Boundary, BoundaryAdmin)
 
+
 class CategorySubmissionAdmin(admin.ModelAdmin):
     list_display = ('id', 'category', "submissionset")
     list_filter = ('submissionset__institution',)
 admin.site.register(CategorySubmission, CategorySubmissionAdmin)
 
+
 class DataCorrectionRequestAdmin(admin.ModelAdmin):
-    list_display = ('user', 'reporting_field', 'date','approved')
+    list_display = ('user', 'reporting_field', 'date', 'approved')
     list_filter = ('approved',)
 admin.site.register(DataCorrectionRequest, DataCorrectionRequestAdmin)
+
 
 class SubcategorySubmissionAdmin(admin.ModelAdmin):
     pass
 admin.site.register(SubcategorySubmission, SubcategorySubmissionAdmin)
 
+
 class CreditUserSubmissionAdmin(admin.ModelAdmin):
     list_filter = ("submission_status", 'credit')
-    list_display = ("credit", "get_institution", "submission_status", "last_updated")
+    list_display = ("credit", "get_institution",
+                    "submission_status", "last_updated")
 admin.site.register(CreditUserSubmission, CreditUserSubmissionAdmin)
+
 
 class UploadSubmissionAdmin(admin.ModelAdmin):
     pass
 admin.site.register(UploadSubmission, UploadSubmissionAdmin)
+
 
 class ResponsiblePartyAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'institution', 'email')
     list_filter = ('institution',)
 admin.site.register(ResponsibleParty, ResponsiblePartyAdmin)
 
+
 class CreditSubmissionInquiryInline(admin.TabularInline):
     model = CreditSubmissionInquiry
-    
+
+
 class FlagAdmin(admin.ModelAdmin):
     model = Flag
 admin.site.register(Flag, FlagAdmin)
 
+
 class SubmissionInquiryAdmin(admin.ModelAdmin):
-    list_display = ('date', 'anonymous', 'last_name', 'first_name', 'submissionset')
-    inlines = [CreditSubmissionInquiryInline,]
+    list_display = ('date', 'anonymous', 'last_name',
+                    'first_name', 'submissionset')
+    inlines = [CreditSubmissionInquiryInline]
 admin.site.register(SubmissionInquiry, SubmissionInquiryAdmin)
 
+
 class ExtensionRequestAdmin(admin.ModelAdmin):
-    list_display = ('submissionset', 'date','user')
+    list_display = ('submissionset', 'date', 'user')
     search_fields = ('user__username',)
 admin.site.register(ExtensionRequest, ExtensionRequestAdmin)
