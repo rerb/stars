2acb9e25d9d79e7ecc2228ac05090c8a714bfa54
Bob Erb <bob.erb@aashe.org>
2013-03-08 17:11:15 -0500

    Rm'ed apps.institutions.tests.views.SubmissionInquiryViewTest.
    Was written back when transition from flash messages to django
    messages framework was made; just tested if messages show up when they
    should.  Written more out of insecurity rather than necessity, and now
    that changes to SubmissionInquiryView have broken it, it's time to
    kill the test rather than fix it.

--- stars/apps/institutions/tests/__init__.py
+++ stars/apps/institutions/tests/__init__.py
@@ -1,13 +1,14 @@
 from stars.apps.institutions.api.test import InstitutionResourceTestCase
-from stars.apps.institutions.tests import stars_account, permissions, \
-     pending_accounts, test_tags
+from stars.apps.institutions.tests import (stars_account, permissions,
+                                           pending_accounts, test_tags)
 from stars.apps.institutions.tests.institution import InstitutionTest
 from stars.apps.institutions.tests.accuracy_inquiry import AccuracyInquiryTest
 from stars.apps.institutions.tests.subscription import SubscriptionTest
-from data_displays_views import AggregateFilterTest, ContentFilterTest, \
-       ScoreFilterTest
-from stars.apps.institutions.tests.views import SubmissionInquiryViewTest, \
-    ActiveInstitutionsViewTest, RatedInstitutionsViewTest, ScorecardViewTest
+from data_displays_views import (AggregateFilterTest, ContentFilterTest,
+                                 ScoreFilterTest)
+from stars.apps.institutions.tests.views import (ActiveInstitutionsViewTest,
+                                                 RatedInstitutionsViewTest,
+                                                 ScorecardViewTest)
 
 
 __test__ = {
@@ -20,7 +21,6 @@ __test__ = {
     'AggregateFilterTest': AggregateFilterTest,
     'ContentFilterTest': ContentFilterTest,
     'ScoreFilterTest': ScoreFilterTest,
-    'SubmissionInquiryViewTest': SubmissionInquiryViewTest,
     'ActiveInstitutionsViewTest': ActiveInstitutionsViewTest,
     'RatedInstitutionsViewTest': RatedInstitutionsViewTest,
     'ScorecardViewTest': ScorecardViewTest,
--- stars/apps/institutions/tests/views.py
+++ stars/apps/institutions/tests/views.py
@@ -19,16 +19,18 @@ from stars.test_factories import InstitutionFactory,\
     SubscriptionFactory, RatingFactory, CreditSetFactory, \
     SubmissionSetFactory
 
+
 class ActiveInstitutionsViewTest(TestCase):
-    """
-        Test the ActiveInstitutionsView
-            this should also test SortableTableView and SortableTableViewWithInstProps
+    """Test the ActiveInstitutionsView
+
+    This should also test SortableTableView and
+    SortableTableViewWithInstProps
     """
     def setUp(self):
-        
+
         today = datetime.date.today()
         td = datetime.timedelta(days=1)
-        
+
         # create 3 active Institutions
         for _ in range(3):
             i = InstitutionFactory(is_participant=True)
@@ -41,24 +43,24 @@ class ActiveInstitutionsViewTest(TestCase):
             s = SubscriptionFactory(end_date=today-td, institution=i)
             i.current_subscription = s
             i.save()
-            
+
     def test_with_client(self):
         """
             This test is enough to cover the code and bring up any
             500 errors. It is not a complete functionality test.
         """
-        
+
         c = Client()
-        
+
         # confirm we get a 200 just querying
         url = "/institutions/"
         response = c.get(url)
         self.assertEqual(response.status_code, 200)
-        
+
         view = views.ActiveInstitutions()
         qs = view.get_queryset()
         self.assertEqual(len(qs), 3)
-        
+
         url = "/institutions/?sort=rating"
         response = c.get(url)
         self.assertEqual(response.status_code, 200)
@@ -70,10 +72,10 @@ class RatedInstitutionsViewTest(TestCase):
             this should also test SortableTableView and SortableTableViewWithInstProps
     """
     def setUp(self):
-        
+
         today = datetime.date.today()
         td = datetime.timedelta(days=1)
-        
+
         # create 3 rated Institutions
         cs = CreditSetFactory()
         count = 0
@@ -88,149 +90,46 @@ class RatedInstitutionsViewTest(TestCase):
             s = SubscriptionFactory(end_date=today-td)
             i.current_subscription = s
             i.save()
-            
+
     def test_with_client(self):
         """
             This test is enough to cover the code and bring up any
             500 errors. It is not a complete functionality test.
         """
-        
+
         c = Client()
-        
+
         # confirm we get a 200 just querying
         url = "/institutions/rated/"
         response = c.get(url)
         self.assertEqual(response.status_code, 200)
-        
+
         view = views.RatedInstitutions()
         qs = view.get_queryset()
         self.assertEqual(len(qs), 4)
-        
+
         url = "/institutions/rated/?sort=rating"
         response = c.get(url)
         self.assertEqual(response.status_code, 200)
-        
+
+
 class ScorecardViewTest(TestCase):
     """
         Test the ScorecardView
-        
+
         This test is enough to cover the code and bring up any
         500 errors. It is not a complete functionality test.
     """
     fixtures = ['rated_submission.json',]
-    
+
     def setUp(self):
-        
+
         pass
-            
+
     def test_with_client(self):
         c = Client()
-        
+
         # confirm we get a 200
         url = "/institutions/rated-college-test/report/2011-01-01/"
         response = c.get(url)
         self.assertEqual(response.status_code, 200)
-
-class SubmissionInquiryViewTest(TestCase):
-
-    def setUp(self):
-        self.request = HttpRequest()
-        self.request.method = 'POST'
-        self.user = User(username='sophisticate')
-        self.user.save()
-        self.request.user = self.user
-        self.request.session = {}
-        self.request.META['REMOTE_ADDR'] = '10.0.0.1'
-
-        self.institution = Institution.objects.create()
-        self.institution.save()
-
-        # Need MessageMiddleware to add the _messages storage backend
-        # to requests
-        self.message_middleware = MessageMiddleware()
-        self.message_middleware.process_request(self.request)
-
-    def tearDown(self):
-        self.user.delete()
-        self.institution.delete()
-
-    def test_process_forms_invalid_recaptcha_response_error_message(self):
-        """Does process_forms display an error msg for invalid recaptcha resp?
-        """
-        submission_inquiry_view = views.SubmissionInquiryView()
-
-        with testfixtures.Replacer() as r:
-            r.replace('stars.apps.institutions.views.SubmissionInquiryView.'
-                      'get_form_list',
-                      lambda x,y,z : ({ 'inquirer_details': MockWithIsValid(True),
-                                   'credit_inquiries': MockWithIsValid(True) },
-                                   dict()))
-            r.replace('stars.apps.institutions.views.captcha',
-                      MockInvalidCaptcha)
-            context, _ = submission_inquiry_view.process_forms(
-                self.request, { 'institution': self.institution })
-        # calling get_success_response just to generate a response
-        # from self.request:
-        response = submission_inquiry_view.get_success_response(
-            self.request, context)
-        soup = BeautifulSoup(response.content)
-        error_message_divs = soup.find_all(
-            'div',
-            {'class': settings.MESSAGE_TAGS[messages.ERROR]})
-        self.assertEqual(len(error_message_divs), 1)
-        self.assertTrue('Captcha Message didn\'t validate' in
-                        error_message_divs[0].text)
-
-    def test_process_forms_invalid_form_error_message(self):
-        """Does process_forms display an error msg for invalid form input?
-        """
-        submission_inquiry_view = views.SubmissionInquiryView()
-
-        with testfixtures.Replacer() as r:
-            r.replace('stars.apps.institutions.views.SubmissionInquiryView.'
-                      'get_form_list',
-                      lambda x,y,z : ({ 'inquirer_details': MockWithIsValid(False),
-                                   'credit_inquiries': MockWithIsValid(True) },
-                                   dict()))
-            r.replace('stars.apps.institutions.views.captcha',
-                      MockValidCaptcha)
-            context, _ = submission_inquiry_view.process_forms(
-                self.request, { 'institution': self.institution })
-        # calling get_success_response just to generate a response
-        # from self.request:
-        response = submission_inquiry_view.get_success_response(
-            self.request, context)
-        soup = BeautifulSoup(response.content)
-        error_message_divs = soup.find_all(
-            'div',
-            {'class': settings.MESSAGE_TAGS[messages.ERROR]})
-        self.assertEqual(len(error_message_divs), 1)
-        self.assertTrue('lease correct the errors' in
-                        error_message_divs[0].text)
-
-class MockWithIsValid(object):
-
-    def __init__(self, is_valid=False, is_valid_lambda=True):
-        if is_valid_lambda:
-            self.is_valid = lambda : is_valid
-        else:
-            self.is_valid = is_valid
-        self.email_address = 'fannymcquiston@example.com'
-        self.error_code = '88'
-
-    def save(self, *args, **kwargs):
-        return MockWithIsValid()
-
-
-class MockInvalidCaptcha(object):
-
-    @classmethod
-    def submit(klass, *args, **kwargs):
-        return MockWithIsValid(is_valid=False, is_valid_lambda=False)
-
-
-class MockValidCaptcha(object):
-
-    @classmethod
-    def submit(klass, *args, **kwargs):
-        return MockWithIsValid(is_valid=True, is_valid_lambda=False)
