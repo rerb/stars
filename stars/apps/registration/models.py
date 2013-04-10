3c8f372a795683641d4f38fd9c373790bf733a77
Bob Erb <bob.erb@aashe.org>
2013-04-10 11:59:20 -0700

    Added percentage attribute to registration.models.ValueDiscount.

--- stars/apps/registration/models.py
+++ stars/apps/registration/models.py
@@ -19,9 +19,21 @@ class DiscountManager(models.Manager):
 
 
 class ValueDiscount(models.Model):
+
     objects = DiscountManager()
     code = models.CharField(max_length=16)
     amount = models.IntegerField(
-        help_text='Amount discounted from registration')
+        help_text='Discount amount')
+    percentage = models.IntegerField(
+        help_text='Discount percentage')
     start_date = models.DateField(help_text='Valid From')
     end_date = models.DateField(help_text='Valid Until')
+
+    def save(self, *args, **kwargs):
+        """Ensure amount and percentage are not both specified.
+        """
+        if (self.amount > 0) and (self.percentage > 0):
+            raise Exception("Only an amount or percentage can "
+                            "be specified, not both.")
+        else:
+            super(ValueDiscount, self).save(*args, **kwargs)
