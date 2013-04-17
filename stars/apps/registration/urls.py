9e2d36004d557312dc030988c8aa9e130cd7e952
Bob Erb <bob.erb@aashe.org>
2013-04-17 13:50:06 -0700

    Rm'ed unused imports, reformatted stars/apps/registration/forms.py.

--- stars/apps/registration/urls.py
+++ stars/apps/registration/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic.simple import direct_to_template
 
-from views import SurveyView
+from .views import SurveyView
+
 
 urlpatterns = patterns(
     'stars.apps.registration.views',
 
-    url(r'^$', 'participant_reg', name='registration'),
+    url(r'^$',
+        'participant_reg',
+        name='registration'),
 
-    url(r'^(?P<institution_slug>[^/]*)/survey/$', SurveyView.as_view(), name='reg_survey')
-
+    url(r'^(?P<institution_slug>[^/]*)/survey/$',
+        SurveyView.as_view(),
+        name='reg_survey')
 )
