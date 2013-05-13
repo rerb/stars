966dc71d7701699548e28bd9f42c0fa74c4a1760
jamstooks <jamstooks@localhost>
2013-05-13 09:47:35 -0400

    Pep8 love for AuthorizeUser model;

--- stars/apps/institutions/data_displays/models.py
+++ stars/apps/institutions/data_displays/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 
+
 class AuthorizedUser(models.Model):
-    
+
     email = models.EmailField()
     start_date = models.DateField()
     end_date = models.DateField()
     member_level = models.BooleanField()
     participant_level = models.BooleanField()
-    
+
     class Meta:
         ordering = ('-start_date','-end_date')
