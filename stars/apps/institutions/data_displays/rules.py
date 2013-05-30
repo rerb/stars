651374cdb56a037635f6217b7606acc2f5dbdb24
jamstooks <jamstooks@localhost>
2013-05-30 17:56:49 -0400

    initial style update to the outline of credits for a report;

---
+++ stars/apps/institutions/data_displays/rules.py
@@ -0,0 +1,50 @@
+import logical_rules
+
+from stars.apps.institutions.rules import user_is_participant
+from stars.apps.institutions.data_displays.models import AuthorizedUser
+
+from datetime import date
+
+
+def user_has_participant_displays(user):
+    """
+        User can access displays that require participant level access
+    """
+    if user_is_participant(user) or user.is_staff:
+        return True
+
+    try:
+        account = AuthorizedUser.objects.get(email=user.email,
+                                             start_date__lte=date.today(),
+                                             end_date__gte=date.today())
+        if account.participant_level:
+            return True
+    except AuthorizedUser.DoesNotExist:
+        pass
+    return False
+logical_rules.site.register("user_has_participant_displays",
+                            user_has_participant_displays)
+
+
+def user_has_member_displays(user):
+    """
+        User can access displays that require member level access
+    """
+    if user.aasheuser.is_member():
+        return True
+
+    # all participants have member display access
+    if user_has_participant_displays(user):
+        return True
+
+    try:
+        account = AuthorizedUser.objects.get(email=user.email,
+                                             start_date__lte=date.today(),
+                                             end_date__gte=date.today())
+        if account.member_level:
+            return True
+    except AuthorizedUser.DoesNotExist:
+        pass
+    return False
+logical_rules.site.register("user_has_member_displays",
+                            user_has_member_displays)
