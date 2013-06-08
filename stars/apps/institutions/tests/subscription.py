5c22f1c9eb978c684edcd7f7223d7c49ebb63c12
Bob Erb <bob.erb@aashe.org>
2013-06-07 18:01:44 -0700

    Fixed institutions/tests/subscription.py tests to conform to recent changes.

--- stars/apps/institutions/tests/subscription.py
+++ stars/apps/institutions/tests/subscription.py
@@ -39,9 +39,10 @@ def get_pay_now_form_data(card_number):
 
 def get_pay_now_form(card_number, subscription=None):
     form_data = get_pay_now_form_data(card_number)
-    form = PayNowForm(instance=subscription, data=form_data)
+    form = PayNowForm(data=form_data)
     if subscription:
-        form.save()
+        subscription.save()
+    form.full_clean()
     return form
 
 
@@ -62,9 +63,9 @@ class SubscriptionTest(TestCase):
         self.member = InstitutionFactory(is_member=True)
         self.nonmember = InstitutionFactory(is_member=False)
 
-    def test__apply_standard_discount(self):
+    def test__apply_early_renewal_discount(self):
         price = 10000
-        self.assertLess(self.subscription._apply_standard_discount(price),
+        self.assertLess(self.subscription._apply_early_renewal_discount(price),
                         price)
 
     #################################
@@ -142,43 +143,54 @@ class SubscriptionTest(TestCase):
         self.assertEqual(start_date, date.today() + timedelta(1))
 
     ####################################
-    # _subscription_discounted() tests:
+    # _qualifies_for_early_renewal_discount() tests:
     ####################################
 
-    def test__subscription_discounted_no_previous_subscription(self):
-        """Does _subscription_discounted work if there's no prev subscription?
+    def test__qualifies_for_early_renewal_discount_no_previous_subscription(
+            self):
+        """Does _qualifies_for_early_renewal_discount work if there's no
+        prev subscription?
         """
-        self.assertFalse(self.subscription._subscription_discounted())
+        self.assertFalse(
+            self.subscription._qualifies_for_early_renewal_discount())
 
-    def test__subscription_discounted_current_subscription(self):
-        """Does _subscription_discounted work when a subscription is current?
+    def test__qualifies_for_early_renewal_discount_current_subscription(self):
+        """Does _qualifies_for_early_renewal_discount work when a
+        subscription is current?
         """
         current_start_date = date.today() - timedelta(days=5)
         current_end_date = date.today() + timedelta(days=5)
         SubscriptionFactory(institution=self.subscription.institution,
                             start_date=current_start_date,
                             end_date=current_end_date)
-        self.assertTrue(self.subscription._subscription_discounted())
+        self.assertTrue(
+            self.subscription._qualifies_for_early_renewal_discount())
 
-    def test__subscription_discounted_recently_lapsed_subscription(self):
-        """Does _subscription_discounted work when subs is recently lapsed?
+    def test__qualifies_for_early_renewal_discount_recently_lapsed_subscription(
+            self):
+        """Does _qualifies_for_early_renewal_discount work when subs is
+        recently lapsed?
         """
         lapsed_start_date = date.today() - timedelta(days=100)
         lapsed_end_date = date.today() - timedelta(days=5)
         SubscriptionFactory(institution=self.subscription.institution,
                             start_date=lapsed_start_date,
                             end_date=lapsed_end_date)
-        self.assertTrue(self.subscription._subscription_discounted())
+        self.assertTrue(
+            self.subscription._qualifies_for_early_renewal_discount())
 
-    def test__subscription_discounted_long_time_lapsed_subscription(self):
-        """Does _subscription_discounted work when a subs lapsed long time ago?
+    def test__qualifies_for_early_renewal_discount_long_time_lapsed_subscription(
+            self):
+        """Does _qualifies_for_early_renewal_discount work when a subs lapsed
+        long time ago?
         """
         lapsed_start_date = date.today() - timedelta(days=1000)
         lapsed_end_date = date.today() - timedelta(days=900)
         SubscriptionFactory(institution=self.subscription.institution,
                             start_date=lapsed_start_date,
                             end_date=lapsed_end_date)
-        self.assertFalse(self.subscription._subscription_discounted())
+        self.assertFalse(
+            self.subscription._qualifies_for_early_renewal_discount())
 
     #############################
     # _apply_promo_code() tests:
@@ -293,30 +305,22 @@ class SubscriptionTest(TestCase):
         else:
             promo_code = None
 
-        self.subscription._subscription_discounted = (
+        self.subscription._qualifies_for_early_renewal_discount = (
             lambda *args : gets_discount)
 
-        price, promo_discount_applied = self.subscription.calculate_price(
-            promo_code=promo_code)
+        prices = self.subscription.calculate_prices(promo_code=promo_code)
 
         price_check = (Subscription.MEMBER_BASE_PRICE if is_member else
                        Subscription.NONMEMBER_BASE_PRICE)
 
+        if gets_discount:
+            price_check = self.subscription._apply_early_renewal_discount(
+                price_check)
+
         if promo_code:
             price_check -= promo_amount
 
-        if gets_discount:
-            price_check = self.subscription._apply_standard_discount(
-                price_check)
-
-        self.assertEqual(price, price_check)
-
-        discount = get_current_discount(code=promo_code)
-
-        if discount:
-            self.assertTrue(promo_discount_applied)
-        else:
-            self.assertFalse(promo_discount_applied)
+        self.assertEqual(prices['total'], price_check)
 
     def test_calculate_price_member_promo_discount(self):
         """Does calculate_price work for member with promo and discount?
@@ -372,8 +376,7 @@ class SubscriptionTest(TestCase):
 
     def test_create_subscription_not_saved(self):
         """Does create() return an unsaved Subscription?"""
-        (subscription, promo_code_applied) = Subscription.create(
-            institution=InstitutionFactory())
+        subscription = Subscription.create(institution=InstitutionFactory())
         self.assertEqual(subscription.id, None)
 
     def test_create_with_promo_code(self):
@@ -382,16 +385,18 @@ class SubscriptionTest(TestCase):
             start_date=date.today(),
             end_date=date.today() + timedelta(days=100),
             amount=10)
-        (subscription, promo_code_applied) = Subscription.create(
+        subscription = None
+        subscription = Subscription.create(
             institution=InstitutionFactory(),
             promo_code=promo.code)
-        self.assertTrue(promo_code_applied)
+        self.assertIsNotNone(subscription)
 
     def test_create_without_promo_code(self):
         """Does create() work when a promo code is not supplied?"""
-        (subscription, promo_code_applied) = Subscription.create(
+        subscription = None
+        subscription = Subscription.create(
             institution=InstitutionFactory())
-        self.assertFalse(promo_code_applied)
+        self.assertIsNotNone(subscription)
 
     ###################################################
     # tests for get_date_range_for_new_subscription():
@@ -545,7 +550,7 @@ class SubscriptionTest(TestCase):
 
            Depends on Subscription.create().
         """
-        (subscription, _) = Subscription.create(
+        subscription = Subscription.create(
             institution=InstitutionFactory())
         form = get_pay_now_form(subscription=subscription,
                                 card_number=GOOD_CREDIT_CARD)
@@ -559,7 +564,7 @@ class SubscriptionTest(TestCase):
 
            Depends on Subscription.create().
         """
-        (subscription, _) = Subscription.create(
+        subscription = Subscription.create(
             institution=InstitutionFactory())
         subscription_price = subscription.amount_due
         form = get_pay_now_form(subscription=subscription,
@@ -576,7 +581,7 @@ class SubscriptionTest(TestCase):
            Depends on Subscription.create().
         """
         initial_payment_count = SubscriptionPayment.objects.count()
-        (subscription, _) = Subscription.create(
+        subscription = Subscription.create(
             institution=InstitutionFactory())
         subscription.purchase(pay_when=Subscription.PAY_LATER,
                               user=UserFactory())
@@ -589,7 +594,7 @@ class SubscriptionTest(TestCase):
            Depends on Subscription.create().
         """
         institution = InstitutionFactory()
-        (subscription, _) = Subscription.create(institution=institution)
+        subscription = Subscription.create(institution=institution)
         subscription.purchase(pay_when=Subscription.PAY_LATER,
                               user=UserFactory())
         self.assertEqual(institution.current_subscription, subscription)
@@ -601,7 +606,7 @@ class SubscriptionTest(TestCase):
            Depends on Subscription.create().
         """
         initial_outgoing_mails = len(mail.outbox)
-        (subscription, _) = Subscription.create(
+        subscription = Subscription.create(
             institution=InstitutionFactory())
         subscription.purchase(pay_when=Subscription.PAY_LATER,
                               user=UserFactory())
@@ -613,7 +618,7 @@ class SubscriptionTest(TestCase):
            Depends on Subscription.create().
         """
         initial_outgoing_mails = len(mail.outbox)
-        (subscription, _) = Subscription.create(
+        subscription = Subscription.create(
             institution=InstitutionFactory())
         form = get_pay_now_form(subscription=subscription,
                                 card_number=BAD_CREDIT_CARD)
@@ -629,7 +634,7 @@ class SubscriptionTest(TestCase):
            Depends on Subscription.create().
         """
         initial_outgoing_mails = len(mail.outbox)
-        (subscription, _) = Subscription.create(
+        subscription = Subscription.create(
             institution=InstitutionFactory())
         subscription.purchase(pay_when=Subscription.PAY_LATER,
                               user=UserFactory())
@@ -643,7 +648,7 @@ class SubscriptionTest(TestCase):
         initial_outgoing_mails = len(mail.outbox)
         institution = InstitutionFactory()
         _ = SubscriptionFactory(institution=institution)
-        (subscription, _) = Subscription.create(institution=institution)
+        subscription = Subscription.create(institution=institution)
         form = get_pay_now_form(subscription=subscription,
                                 card_number=GOOD_CREDIT_CARD)
         subscription.purchase(pay_when=Subscription.PAY_NOW,
@@ -657,7 +662,7 @@ class SubscriptionTest(TestCase):
            Depends on Subscription.create().
         """
         initial_outgoing_mails = len(mail.outbox)
-        (subscription, _) = Subscription.create(
+        subscription = Subscription.create(
             institution=InstitutionFactory())
         form = get_pay_now_form(subscription=subscription,
                                 card_number=GOOD_CREDIT_CARD)
@@ -678,7 +683,7 @@ class SubscriptionTest(TestCase):
         user_email = 'teddy@kozinski.nom'
         institution = InstitutionFactory(contact_email=contact_email)
         user = UserFactory(email=user_email)
-        (subscription, _) = Subscription.create(institution=institution)
+        subscription = Subscription.create(institution=institution)
         subscription.purchase(pay_when=Subscription.PAY_LATER,
                               user=user)
         message = mail.outbox.pop()
