d596c2cc5ac1e55c1ea2b5aad6092629b441e199
jamstooks <jamstooks@localhost>
2013-07-11 14:50:59 -0400

    Updates to excel formatting;

--- stars/apps/submissions/export/excel.py
+++ stars/apps/submissions/export/excel.py
@@ -15,10 +15,15 @@ def get_summary_sheet(submission, sheet):
         Builds the first sheet of an exported report that
         has the summary of the submission
     """
-    r, c = 0, 0
-    sheet.col(c).width = 8000
+    boldFont = xlwt.Font()
+    boldFont.bold = True
+    boldStyle = xlwt.XFStyle()
+    boldStyle.font = boldFont
 
-    sheet.write_merge(r, r, c, c + 1, submission.institution.name)
+    r, c = 0, 0
+    min_width = get_width(len(submission.institution.name))
+
+    sheet.write_merge(r, r, c, c + 1, submission.institution.name, boldStyle)
     r += 1
 
     sheet.write_merge(r, r, c, c + 1, 'STARS Report Summary')
@@ -37,19 +42,28 @@ def get_summary_sheet(submission, sheet):
     r += 1
 
     for cs in submission.categorysubmission_set.all():
+        if min_width < get_width(len(cs.category.title)):
+            min_width = get_width(len(cs.category.title))
         sheet.write(r, c, cs.category.title)
         sheet.write(r, c + 1, cs.get_STARS_score())
         r += 1
+
+    sheet.col(c).width = min_width
 
 
 def build_category_summary_sheet(category, sheet):
     """
         Builds the summary sheet for a category
     """
-    r, c = 0, 0
-    sheet.col(c).width = 12000
+    boldFont = xlwt.Font()
+    boldFont.bold = True
+    boldStyle = xlwt.XFStyle()
+    boldStyle.font = boldFont
 
-    sheet.write_merge(r, r, c, c + 1, category.category.title)
+    r, c = 0, 0
+    min_width = 12000
+
+    sheet.write_merge(r, r, c, c + 1, category.category.title, boldStyle)
     r += 1
 
     sheet.write(r, c, "Credit Number and Title")
@@ -59,9 +73,14 @@ def build_category_summary_sheet(category, sheet):
     r += 2
 
     for sub in category.subcategorysubmission_set.all():
-        sheet.write(r, c, sub.subcategory.title)
+        if min_width < get_width(len(sub.subcategory.title)):
+            min_width = get_width(len(sub.subcategory.title))
+
+        sheet.write(r, c, sub.subcategory.title, boldStyle)
         r += 1
         for cs in sub.creditusersubmission_set.all():
+            if min_width < get_width(len(unicode(cs.credit))):
+                min_width = get_width(len(unicode(cs.credit)))
             sheet.write(r, c, unicode(cs.credit))
             sheet.write(r, c + 1, cs.assessed_points)
             sheet.write(r, c + 2, cs.credit.point_value)
@@ -69,25 +88,54 @@ def build_category_summary_sheet(category, sheet):
             r += 1
         r += 1
 
+    sheet.col(c).width = min_width
+    for c in range(1, 5):
+        sheet.col(c).width = 5000
+
 
 def build_category_data_sheet(category, sheet):
     """
         Builds the data sheet for a category
     """
-    r, c = 0, 0
-    sheet.col(c + 1).width = 10000
-    sheet.col(c + 2).width = 10000
-    sheet.col(c + 3).width = 10000
+
+    col_widths = [3000, 5000, 10000, 15000]
+    max_widths = [8000, 20000, 20000, 20000]
+
+    def update_width(column, content):
+        if col_widths[column] < get_width(len(content)):
+            col_widths[column] = get_width(len(content))
+        if col_widths[column] > max_widths[column]:
+            col_widths[column] = max_widths[column]
+
+    borders = xlwt.Borders()
+    borders.left = 1
+    borders.right = 1
+    borders.top = 1
+    borders.bottom = 1
+
+    borderedStyle = xlwt.XFStyle()
+    borderedStyle.borders = borders
+
+    boldFont = xlwt.Font()
+    boldFont.bold = True
+
+    boldBorderedStyle = xlwt.XFStyle()
+    boldBorderedStyle.borders = borders
+    boldBorderedStyle.font = boldFont
 
     alignment = xlwt.Alignment()
     alignment.vert = xlwt.Alignment.VERT_CENTER
-    style = xlwt.XFStyle()
-    style.alignment = alignment
+    centeredBorderedStyle = xlwt.XFStyle()
+    centeredBorderedStyle.borders = borders
+    centeredBorderedStyle.alignment = alignment
 
-    sheet.write(r, c, "Credit")
-    sheet.write(r, c + 1, "Credit Title")
-    sheet.write(r, c + 2, "Reporting Field")
-    sheet.write(r, c + 3, "Response")
+    r, c = 0, 0
+
+    sheet.write(r, c, "Credit", boldBorderedStyle)
+    update_width(c, "Credit")
+    sheet.write(r, c + 1, "Credit Title", boldBorderedStyle)
+    sheet.write(r, c + 2, "Reporting Field", boldBorderedStyle)
+    sheet.write(r, c + 3, "Response", boldBorderedStyle)
 
     r += 1
     for ss in category.subcategorysubmission_set.all()[:1]:
@@ -95,13 +143,19 @@ def build_category_data_sheet(category, sheet):
 
             responses = cs.get_submission_fields()
 
-            sheet.write_merge(r, r + len(responses) - 1, c, c, cs.credit.identifier, style=style)
-            sheet.write_merge(r, r + len(responses) - 1, c + 1, c + 1, cs.credit.title, style=style)
+            sheet.write_merge(r, r + len(responses) - 1, c, c, cs.credit.identifier, style=centeredBorderedStyle)
+            update_width(c, cs.credit.identifier)
+            sheet.write_merge(r, r + len(responses) - 1, c + 1, c + 1, cs.credit.title, style=centeredBorderedStyle)
+            update_width(c + 1, cs.credit.title)
 
             for f in responses:
-                sheet.write(r, c + 2, f.documentation_field.title)
-                sheet.write(r, c + 3, f.get_human_value())
+                sheet.write(r, c + 2, f.documentation_field.title, borderedStyle)
+                update_width(c + 2, f.documentation_field.title)
+                sheet.write(r, c + 3, f.get_human_value(), borderedStyle)
                 r += 1
+
+    for c, w in enumerate(col_widths):
+        sheet.col(c).width = w
 
 
 def build_report_export(submission):
@@ -130,6 +184,6 @@ def build_report_export(submission):
     wb.save(tempfile.name)
     return tempfile.name
 
-# from stars.apps.submissions.models import SubmissionSet
-# ss = SubmissionSet.objects.get(pk=1157)
-# build_report_export(ss)
+from stars.apps.submissions.models import SubmissionSet
+ss = SubmissionSet.objects.get(pk=1157)
+build_report_export(ss)
