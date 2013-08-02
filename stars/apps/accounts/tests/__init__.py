4ea7c9ded891b73ed10407692f231d88331cc4e1
Bob Erb <bob.erb@aashe.org>
2013-08-02 14:11:11 -0700

    Rm'ed apps/accounts/tests/... that only tested logging.
    They wouldn't compile anyway.  So they messed up "manage.py test".

--- stars/apps/accounts/tests/__init__.py
+++ stars/apps/accounts/tests/__init__.py
@@ -1,18 +1,10 @@
 import account_mixin, users
-from aashe_auth_backend import AASHEAuthBackendTest
-from submission_mixin import SubmissionMixinTest
-from utils import UtilsTest
-from views import ViewsTest
 from decorators import DecoratorsTest
 from mixins import StarsMixinTest, AccountMixinTest
 
 __test__ = {
     'account_mixin': account_mixin,
     'users': users,
-    'AASHEAuthBackendTest': AASHEAuthBackendTest,
-    'SubmissionMixinTest': SubmissionMixinTest,
-    'UtilsTest': UtilsTest,
-    'ViewsTest': ViewsTest,
     'DecoratorsTest': DecoratorsTest,
     'StarsMixinTest': StarsMixinTest,
     'AccountMixinTest': AccountMixinTest
