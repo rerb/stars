bfb18302c9a76c34f8d0807a05d1c55dceaba959
Matt Thomas <mthomas@aashe.org>
2013-08-05 14:10:54 -0400

    adding slugless url pattern

--- stars/apps/old_cms/urls.py
+++ stars/apps/old_cms/urls.py
@@ -13,6 +13,10 @@ urlpatterns = patterns('',
         OldPathRedirectView.as_view(),
         name='cms-old-path-redirect'),
 
+    url(r'^(?P<article_slug>[^\/]+).html$',
+        ArticleDetailView.as_view(),
+        name='cms-article-detail'),
+
     url(r'^(?P<category_slug>[^\/]+)/(?P<article_slug>[^\/]+).html$',
         ArticleDetailView.as_view(),
         name='cms-article-detail'),
