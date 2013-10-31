d074c0dc52bfe8bbbad301f0e67cd63385b6d1ee
Bob Erb <bob.erb@aashe.org>
2013-10-31 10:21:43 -0700

    Rm'ed test of replaced functionality, stars/apps/accounts/tests/users.py.

--- stars/apps/accounts/tests/__init__.py
+++ stars/apps/accounts/tests/__init__.py
@@ -1,10 +1,9 @@
-import account_mixin, users
+import account_mixin
 from decorators import DecoratorsTest
 from mixins import StarsMixinTest, AccountMixinTest
 
 __test__ = {
     'account_mixin': account_mixin,
-    'users': users,
     'DecoratorsTest': DecoratorsTest,
     'StarsMixinTest': StarsMixinTest,
     'AccountMixinTest': AccountMixinTest
