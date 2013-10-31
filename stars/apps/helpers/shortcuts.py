5f77e9cfc5d71294efcd7efdba8c2a71f085454a
Bob Erb <bob.erb@aashe.org>
2013-10-31 15:03:50 -0700

    Rm'ed unused imports from stars/apps/helpers/shortcuts.py.

--- stars/apps/helpers/shortcuts.py
+++ stars/apps/helpers/shortcuts.py
@@ -5,9 +5,6 @@ modelled on Django's shortcuts module
 from logging import getLogger
 
 from django.template import loader, Context
-from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponse, Http404
-from stars.apps.helpers import exceptions
 
 logger = getLogger('stars')
 
@@ -17,12 +14,15 @@ def render_to_any_response(HttpResponseClass, *args, **kwargs):
     as an argument, so we can render custom 404 or 500 pages.
     """
     httpresponse_kwargs = {'mimetype': kwargs.pop('mimetype', None)}
-    return HttpResponseClass(loader.render_to_string(*args, **kwargs), **httpresponse_kwargs)
+    return HttpResponseClass(loader.render_to_string(*args, **kwargs),
+                             **httpresponse_kwargs)
 
 
 def render_help_text(help_text, as_tooltip=True):
-    """ Use the template tag normally used to render help text in templates to render and return formatted help text """
+    """ Use the template tag normally used to render help text in
+    templates to render and return formatted help text """
     from stars.apps.helpers.templatetags.help import show_help_text
 
     help_template = loader.get_template('helpers/tags/help_text.html')
-    return help_template.render(Context(show_help_text(help_text, as_tooltip)))
+    return help_template.render(Context(show_help_text(help_text,
+                                                       as_tooltip)))
