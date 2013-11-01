2c102fa9d3606545d5976aca17535491a510addf
Bob Erb <bob.erb@aashe.org>
2013-11-01 14:59:16 -0700

    Fixed migrations tests.

--- stars/apps/migrations/tests/creditset_migration_tests.py
+++ stars/apps/migrations/tests/creditset_migration_tests.py
@@ -19,35 +19,48 @@ class TestMigration(TestCase):
         pass
     
     def testGetLatest(self):
-        print >> sys.stderr, "Test Latest"
         cs = CreditSet.objects.get_latest()
         self.assertEqual(cs.id, 2)
         
     
     def testCopyCreditSet(self):
-        print >> sys.stderr, "Test: copy creditset"
         cs = CreditSet.objects.get(pk=2)
         new_cs = migrate_creditset(cs, "1.1", date.today())
         
-        print "Categories: %d" % new_cs.category_set.count()
         self.assertEqual(new_cs.category_set.count(), cs.category_set.count())
         
-        print "Subcategories: %d" % Subcategory.objects.filter(category__creditset=new_cs).count()
-        self.assertEqual(Subcategory.objects.filter(category__creditset=new_cs).count(), Subcategory.objects.filter(category__creditset=cs).count())
+        self.assertEqual(
+            Subcategory.objects.filter(category__creditset=new_cs).count(),
+            Subcategory.objects.filter(category__creditset=cs).count())
         
-        print "Credits: %d" % Credit.objects.filter(subcategory__category__creditset=new_cs).count()
-        self.assertEqual(Credit.objects.filter(subcategory__category__creditset=new_cs).count(), Credit.objects.filter(subcategory__category__creditset=cs).count())
+        self.assertEqual(
+            Credit.objects.filter(
+                subcategory__category__creditset=new_cs).count(),
+            Credit.objects.filter(
+                subcategory__category__creditset=cs).count())
         
-        print "Applicability Reasons: %d" % ApplicabilityReason.objects.filter(credit__subcategory__category__creditset=new_cs).count()
-        self.assertEqual(ApplicabilityReason.objects.filter(credit__subcategory__category__creditset=new_cs).count(), ApplicabilityReason.objects.filter(credit__subcategory__category__creditset=cs).count())
+        self.assertEqual(
+            ApplicabilityReason.objects.filter(
+                credit__subcategory__category__creditset=new_cs).count(),
+            ApplicabilityReason.objects.filter(
+                credit__subcategory__category__creditset=cs).count())
         
-        print "Documentation Fields: %d" % DocumentationField.objects.filter(credit__subcategory__category__creditset=new_cs).count()
-        self.assertEqual(DocumentationField.objects.filter(credit__subcategory__category__creditset=new_cs).count(), DocumentationField.objects.filter(credit__subcategory__category__creditset=cs).count())
+        self.assertEqual(
+            DocumentationField.objects.filter(
+                credit__subcategory__category__creditset=new_cs).count(),
+            DocumentationField.objects.filter(
+                credit__subcategory__category__creditset=cs).count())
         
-        print "Choices: %d" % Choice.objects.filter(documentation_field__credit__subcategory__category__creditset=new_cs).count()
-        self.assertEqual(Choice.objects.filter(documentation_field__credit__subcategory__category__creditset=new_cs).count(), Choice.objects.filter(documentation_field__credit__subcategory__category__creditset=cs).count())
+        self.assertEqual(
+            Choice.objects.filter(
+                documentation_field__credit__subcategory__category__creditset=new_cs).count(),
+            Choice.objects.filter(
+                documentation_field__credit__subcategory__category__creditset=cs).count())
         
-        print "CreditTestSubmissions: %d" % CreditTestSubmission.objects.filter(credit__subcategory__category__creditset=new_cs).count()
-        self.assertEqual(CreditTestSubmission.objects.filter(credit__subcategory__category__creditset=new_cs).count(), CreditTestSubmission.objects.filter(credit__subcategory__category__creditset=cs).count())
+        self.assertEqual(
+            CreditTestSubmission.objects.filter(
+                credit__subcategory__category__creditset=new_cs).count(),
+            CreditTestSubmission.objects.filter(
+                credit__subcategory__category__creditset=cs).count())
         
         
