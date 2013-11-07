b17c464d146d8bbf5d111a59ae6533ff71abf21f
jamstooks <jamstooks@localhost>
2013-11-07 17:41:07 -0400

    Added null options to SubmissionSet admin for migrated_from and rating fields;

---
+++ stars/apps/old_cms/models.py
@@ -0,0 +1,136 @@
+import time
+from django.db import models, IntegrityError
+from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
+from django.db.models.signals import post_save
+
+from stars.apps.helpers.utils import invalidate_template_cache
+
+
+class AbstractContent(models.Model):
+    ordinal = models.SmallIntegerField(default=0)
+    published = models.BooleanField(default=True)
+    timestamp = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ('ordinal','title')
+
+    def __str__(self):
+        return self.title
+
+    def get_django_admin_url(self):
+        return "/_ad/%s/%s/%d" % (
+                                    self._meta.app_label,
+                                    self._meta.module_name,
+                                    self.id,
+                                )
+
+class CategoryMixin(models.Model):
+
+    title = models.CharField(max_length=64)
+    slug = models.SlugField(help_text='This is a URL-friendly version of the title. Do not change unless you want to change the link')
+    content = models.TextField(blank=True, null=True, help_text='If left blank, the page will be populated with the teaser text from all the articles.')
+
+    class Meta:
+        abstract=True
+
+    def get_articles(self):
+        return self.newarticle_set.filter(published=True).order_by('ordinal')
+
+class Category(CategoryMixin, AbstractContent):
+    """
+        Django-hosted CMS top-level category
+    """
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def get_absolute_url(self):
+        return "/pages/%s/" % self.slug
+
+    def get_published_subcategories(self):
+        return self.subcategory_set.filter(published=True).order_by('ordinal')
+
+class Subcategory(CategoryMixin, AbstractContent):
+    parent = models.ForeignKey(Category)
+
+    class Meta:
+        verbose_name_plural = "Subcategories"
+
+    def get_absolute_url(self):
+        return "%s%s/" % (self.parent.get_absolute_url(), self.slug)
+
+    def save(self, *args, **kwargs):
+        super(Subcategory, self).save(*args, **kwargs)
+        self.parent.save() # Update timestamp for caching
+
+    def update_timestamps(self):
+        """
+            All categories get new timestamps because I can't tell
+            which category this article used to belong to.
+            Of course, I could if I wrote a custom form for the admin,
+            but that's not the only place these could be saved
+        """
+        for cat in Category.objects.all():
+            cat.save()
+
+    def get_published_articles(self):
+        return self.newarticle_set.filter(published=True).order_by('ordinal')
+
+class NewArticle(AbstractContent):
+    """
+        Django-hosted CMS article
+    """
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    content = models.TextField()
+    teaser = models.TextField(blank=True, null=True)
+    categories = models.ManyToManyField(Category, blank=True, null=True)
+    # subcategories = models.ManyToManyField(Subcategory, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    irc_id = models.IntegerField(blank=True, null=True, help_text='Only necessary for pages that used to exist in the IRC. New pages will not need this.')
+
+    class Meta:
+        ordering = ('ordinal', 'title', 'timestamp',)
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+
+    def get_absolute_url(self):
+        if self.categories.count() > 0:
+            return "%s%s.html" % (self.categories.all()[0].get_absolute_url(), self.slug)
+        return "#"
+
+    def update_timestamps(self):
+        """
+            All categories and subcategories get new timestamps
+            because I can't tell which category this article
+            used to belong to.
+            Of course, I could if I wrote a custom form for the admin,
+            but that's not the only place these could be saved
+        """
+        # for sub in Subcategory.objects.all():
+        #     sub.save()
+        for cat in Category.objects.all():
+            cat.save()
+
+def post_save_article_rec(sender, instance, **kwargs):
+    instance.update_timestamps()
+post_save.connect(post_save_article_rec, sender=NewArticle)
+
+def post_save_sub_rec(sender, instance, **kwargs):
+    instance.update_timestamps()
+post_save.connect(post_save_sub_rec, sender=Subcategory)
+
+class HomepageUpdate(AbstractContent):
+    """
+        Update that appears on homepage feed.
+    """
+    title = models.CharField(max_length=255)
+    link = models.URLField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('ordinal', 'title', 'timestamp',)
+        verbose_name = "Homepage Update"
+        verbose_name_plural = "Homepage Updates"
---
+++ stars/test_factories/misc_factories.py
@@ -0,0 +1,24 @@
+import time
+
+from django.contrib.auth.models import User
+import factory
+
+
+class UserFactory(factory.DjangoModelFactory):
+    FACTORY_FOR = User
+
+    username = factory.Sequence(
+        lambda i: 'testuser-{0}.{1}'.format(i, time.time()))
+    password = 'test'
+    email = factory.LazyAttribute(
+        lambda o: '{0}@example.com'.format(o.username))
+
+    @classmethod
+    def _prepare(cls, create, **kwargs):
+        password = kwargs.pop('password', None)
+        user = super(UserFactory, cls)._prepare(create, **kwargs)
+        if password:
+            user.set_password(password)
+            if create:
+                user.save()
+        return user
