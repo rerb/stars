1d4b621d25a5a0056097d79e5994289d616a0863
Bob Erb <bob.erb@aashe.org>
2014-01-05 20:56:56 -0800

    Added help text for `Use metric system` setting.

--- stars/apps/tool/forms.py
+++ stars/apps/tool/forms.py
@@ -12,9 +12,14 @@ class SettingsUpdateForm(LocalizedModelFormMixin):
         on Institution.
     """
     prefers_metric_system = django.forms.BooleanField(
-        label="Use metric system", required=False)
+        label=("Use metric system. When checked, the Reporting Tool treats "
+               "measured amounts as metric quantities. This is a system-"
+               "wide setting; changing it effects all users. If others are "
+               "using the Reporting Tool now, they should log out before "
+               "you change this setting. You should be the only one "
+               "logged into STARS when you change this."),
+        required=False)
 
     class Meta:
         model = Institution
         fields = ['prefers_metric_system']
-
