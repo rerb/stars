0067993ad894e12608ce987a105c90fe1fff9cbd
jamstooks <jamstooks@localhost>
2014-09-22 12:46:30 -0300

    fixes to spider graph;

--- stars/apps/submissions/cron.py
+++ stars/apps/submissions/cron.py
@@ -2,8 +2,8 @@ from stars.apps.institutions.models import *
 #from stars.apps.credits.models import *
 #from stars.apps.submissions.models import *
 
-from tasks import update_pie_api_cache
-from tasks import expireRatings
+from stars.apps.submissions.tasks import update_pie_api_cache
+from stars.apps.submissions.tasks import expireRatings
 
 update_pie_api_cache()
 expireRatings()
