b676d9dd9300db5e75a6cc5ad4595a459478edff
jamstooks <jamstooks@localhost>
2014-10-22 00:12:13 -0300

    added the script to migrate all institutions to 2.0;

---
+++ stars/tests/upgrade_to_2.0.py
@@ -0,0 +1,26 @@
+from django.contrib.auth.models import User
+
+from stars.apps.submissions.tasks import perform_migration
+from stars.apps.credits.models import CreditSet
+from stars.apps.institutions.models import Institution
+
+user = User.objects.get(email="ben@aashe.org")
+cs2 = CreditSet.objects.get(pk=6)
+
+# Find all the institutions that need to be migrated, and migrate them
+# exclude the ITESM institutions
+
+qs = Institution.objects.all()
+
+print "Total Institutions: %d" % qs.count()
+
+qs = qs.exclude(name__startswith="Instituto Tecno")
+qs = qs.exclude(name="ITESM")
+
+count = 0
+for i in qs.order_by("name"):
+    if i.current_submission.creditset.version != "2.0":
+        print i.current_submission
+        count += 1
+        perform_migration.delay(i.current_submission, cs2, user)
+print "Queued: %d" % count
