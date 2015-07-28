3ca7de4c0bdf4442d1f2f4cf3eab444f65b9a654
rerb <bob.erb@aashe.org>
2015-07-28 11:40:31 -0700

    Simplified data_displays/tests/__init__.py.

--- stars/apps/institutions/data_displays/tests/__init__.py
+++ stars/apps/institutions/data_displays/tests/__init__.py
@@ -1,18 +1,3 @@
-from stars.apps.institutions.data_displays.tests.models import (
-    AuthorizedUserTestCase)
-from stars.apps.institutions.data_displays.tests.filters import (
-    FilterTestCase)
-from stars.apps.institutions.data_displays.tests.views import (
-    AggregateFilterTestCase,
-    ContentFilterTestCase,
-    DashboardTestCase,
-    ScoreFilterTestCase)
-
-__test__ = {
-    'AggregateFilterTestCase,': AggregateFilterTestCase,
-    'AuthorizedUserTestCase': AuthorizedUserTestCase,
-    'ContentFilterTestCase': ContentFilterTestCase,
-    'DashboardTestCase': DashboardTestCase,
-    'FilterTestCase': FilterTestCase,
-    'ScoreFilterTestCase': ScoreFilterTestCase
-}
+from stars.apps.institutions.data_displays.tests.models import *
+from stars.apps.institutions.data_displays.tests.filters import *
+from stars.apps.institutions.data_displays.tests.views import *
