15e786e53450b5776c40fd2d5b92d0ff7e02d392
rerb <bob.erb@aashe.org>
2015-09-14 16:51:21 -0400

    Added logging to load_subcategory_org_type_average_points.

--- stars/apps/submissions/utils.py
+++ stars/apps/submissions/utils.py
@@ -1,20 +1,29 @@
+from logging import getLogger
+
 from stars.apps.credits.models import Subcategory
 from stars.apps.institutions.models import Institution
 from stars.apps.submissions.models import SubcategoryOrgTypeAveragePoints
 
 
-def load_subcategory_average_points_cache():
+logger = getLogger('stars')
 
+
+def load_subcategory_org_type_average_points():
+    """Populate SubcategoryOrgTypeAveragePoints and calculate the averages.
+    """
     org_types = Institution.get_org_types()
     subcategories = Subcategory.objects.all()
     for subcategory in subcategories:
         for org_type in org_types:
+            logger.info('loading subcategory {subcategory} '
+                        'for {org_type}'.format(subcategory=subcategory.title,
+                                                org_type=org_type))
             try:
-                cached_average = SubcategoryOrgTypeAveragePoints.objects.get(
+                average = SubcategoryOrgTypeAveragePoints.objects.get(
                     subcategory=subcategory,
                     org_type=org_type)
             except SubcategoryOrgTypeAveragePoints.DoesNotExist:
-                cached_average = SubcategoryOrgTypeAveragePoints.objects.create(
+                average = SubcategoryOrgTypeAveragePoints.objects.create(
                     subcategory=subcategory,
                     org_type=org_type)
-            cached_average.calculate()
+            average.calculate()
