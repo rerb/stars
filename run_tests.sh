a128fff79be12b26eeaa066b532915e794b66a68
rerb <bob.erb@aashe.org>
2015-11-23 11:48:28 -0500

    Added 'manage' to run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-coverage run manage.py test test_factories helpers registration data_displays institutions
+coverage run manage.py test test_factories helpers registration data_displays institutions manage
