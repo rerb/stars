7bcdf0d8196ce78e9252d9c07d88fd990312df96
rerb <bob.erb@aashe.org>
2015-12-08 12:54:10 -0500

    Spread a little PEP8 love on submit_tags.py

--- stars/apps/tool/my_submission/templatetags/submit_tags.py
+++ stars/apps/tool/my_submission/templatetags/submit_tags.py
@@ -1,4 +1,3 @@
-from django.utils.html import strip_spaces_between_tags, escape
 from django import template
 register = template.Library()
 
@@ -26,21 +25,24 @@ def format_available_points(creditsubmission, use_cache=False):
 
 @register.inclusion_tag('tool/submissions/tags/crumbs.html')
 def show_submit_crumbs(object):
-    """ Displays the crumb navigation for a particular object in the Submission Tool """
+    """ Displays the crumb navigation for a particular object in the
+        Submission Tool
+    """
     object_set = []
     parent = object
     while parent:
         object_set.insert(0, parent)
         parent = parent.get_parent()
-        
+
     return {'object_set': object_set}
+
 
 @register.inclusion_tag('tool/submissions/tags/progress_icon.html')
 def show_progress_icon(category, size_class=''):
     """ Displays a progress bar showing the percent complete """
-    
+
     z = category.__class__.__name__
-    
+
     if z == 'int':
         # if the int is a percent
         numerator = ''
@@ -52,11 +54,11 @@ def show_progress_icon(category, size_class=''):
         numerator = category.get_finished_credit_count()
         denominator = category.get_total_credits()
         if numerator > 0:
-            percentage = float(numerator)/float(denominator) * 100;
+            percentage = float(numerator) / float(denominator) * 100
         else:
-            percentage = 0 # Fix for css issues
+            percentage = 0  # Fix for css issues
         title = "%.0f / %.0f" % (numerator, denominator)
-        
+
         if z == "CategorySubmission":
             content = "%.0f / %.0f" % (numerator, denominator)
             caption = "Progress"
@@ -64,7 +66,7 @@ def show_progress_icon(category, size_class=''):
             content = ""
             caption = "%.0f / %.0f Progress" % (numerator, denominator)
     return {
-        'percentage': percentage, 
+        'percentage': percentage,
         'numerator': numerator,
         'denominator': denominator,
         'content': content,
@@ -72,16 +74,20 @@ def show_progress_icon(category, size_class=''):
         'title': title,
     }
 
+
 class Icon(object):
     __slots__ = ('title', 'file', 'alt', 'size_class')
-    
+
+
 @register.inclusion_tag('tool/submissions/tags/status_icon.html')
 def show_status_icon(credit, white=0):
     """ Displays an icon representing credit status """
     from stars.apps.submissions.models import CREDIT_SUBMISSION_STATUS_ICONS
     title = credit.get_submission_status_display
-    icon, alt = CREDIT_SUBMISSION_STATUS_ICONS.get(credit.submission_status, (None, None))
+    icon, alt = CREDIT_SUBMISSION_STATUS_ICONS.get(credit.submission_status,
+                                                   (None, None))
     return {'icon': icon, 'white': white, 'title': title}
+
 
 @register.inclusion_tag('tool/submissions/tags/payment_icon.html')
 def show_payment_type_icon(payment, size_class=''):
@@ -90,14 +96,18 @@ def show_payment_type_icon(payment, size_class=''):
     icon = Icon()
     icon.size_class = size_class
     icon.alt = payment.method
-    icon.file, icon.title = PAYMENT_TYPE_ICONS.get(payment.method, (None, None))
+    icon.file, icon.title = PAYMENT_TYPE_ICONS.get(payment.method,
+                                                   (None, None))
     return {'icon': icon}
 
-@register.inclusion_tag('tool/submissions/tags/documentation_field_inside_table.html')
+
+@register.inclusion_tag(
+    'tool/submissions/tags/documentation_field_inside_table.html')
 def show_submission_field_control(form_list, id):
     """ Displays the submission form for a documentation field """
     form = form_list[id]
-    return{"documentation_field":form.instance.documentation_field, "field_form":form }
+    return{"documentation_field": form.instance.documentation_field,
+           "field_form": form}
 
 
 def _get_form(doc_field, submission_form):
@@ -108,6 +118,7 @@ def _get_form(doc_field, submission_form):
             form = ff['form']
     return form
 
+
 @register.inclusion_tag('tool/submissions/tags/documentation_field_form.html')
 def show_submission_form_for_field(doc_field, submission_form):
     """ Displays the submission form for a documentation field """
@@ -117,7 +128,9 @@ def show_submission_form_for_field(doc_field, submission_form):
            "field_template": "tool/submissions/tags/tabular_field.html",
            "submission_form": submission_form}
 
-@register.inclusion_tag('tool/submissions/tags/documentation_field_inside_table.html')
+
+@register.inclusion_tag(
+    'tool/submissions/tags/documentation_field_inside_table.html')
 def show_submission_form_for_field_inside_table(doc_field, submission_form):
     """ Displays the submission form for a documentation field """
     form = _get_form(doc_field, submission_form)
