ab06180e17633d44c5d357709b312234b8d46d55
Benjamin Stookey <ben.stookey@gmail.com>
2016-03-22 22:11:55 -0600

    Removed troublesome global imports in factories;

--- stars/test_factories/__init__.py
+++ stars/test_factories/__init__.py
@@ -1,8 +1,8 @@
-from accounts_factories import *
-from auth_factories import *
+from accounts_factories import UserProfileFactory
+from auth_factories import AASHEAccountFactory
 from credits_factories import *
 from institutions_factories import *
-from misc_factories import *
+from misc_factories import UserFactory, AASHEUserFactory
 from notifications_factories import *
 from organizations_factories import *
 from registration_factories import *
