5e6e55f1932a312febea24c5c7c9711a88f7ed6f
rerb <bob.erb@aashe.org>
2016-04-30 23:08:48 -0400

    Add credits to run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-coverage run manage.py test test_factories helpers registration data_displays institutions submissions manage my_submission
+coverage run manage.py test test_factories helpers registration data_displays institutions submissions manage my_submission credits
