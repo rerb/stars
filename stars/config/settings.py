175d17d9210eeef268d056ea7832ef1e3399b901
rerb <bob.erb@aashe.org>
2016-06-06 16:24:36 -0400

    Don't cache templates if DEBUG

--- stars/config/settings.py
+++ stars/config/settings.py
@@ -81,10 +81,14 @@ STATICFILES_FINDERS = (
 SECRET_KEY = 'omxxweql@m7!@yh5a-)=f^_xo*(m2+gaz#+8dje)e6wv@q$v%@'
 
 TEMPLATE_LOADERS = (
-    ('django.template.loaders.cached.Loader', (
-        'django.template.loaders.filesystem.Loader',
-        'django.template.loaders.app_directories.Loader')),
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader'
 )
+
+if not DEBUG:  # Cache templates.
+    TEMPLATE_LOADERS = (
+        ('django.template.loaders.cached.Loader',
+         TEMPLATE_LOADERS))
 
 LANGUAGES = [
     ('en', 'English'),
