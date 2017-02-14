adf0a8ba66bdf6e894fb0d04c06e4da7a7c67ac9
Robert Erb <bob.erb@gmail.com>
2017-02-14 11:07:03 -0500

    Update email sent via Submission Review process

--- stars/apps/accounts/urls.py
+++ stars/apps/accounts/urls.py
@@ -1,16 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 
-# Django Docs:
-# http://docs.djangoproject.com/en/dev/topics/auth/
 
 urlpatterns = patterns(
     '',
-
-    #(r'^login/$', 'django.contrib.accounts.views.login', {'template_name': 'auth/login.html'}),
     (r'^login/$', 'stars.apps.accounts.views.login_view'),
     (r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
 
-    # Terms of Service
     (r'^tos/$', 'stars.apps.accounts.views.terms_of_service'),
 
     url(r'^select-school/(?P<institution_id>\d+)/$',
