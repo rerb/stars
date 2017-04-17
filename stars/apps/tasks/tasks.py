9163b0a0087a8791fece0a89f0e21676d9f9c92f
Robert Erb <bob.erb@gmail.com>
2017-04-17 16:27:01 -0400

    Fix notifications task

--- stars/apps/tasks/tasks.py
+++ stars/apps/tasks/tasks.py
@@ -2,9 +2,8 @@ from __future__ import absolute_import
 
 from celery import shared_task
 
-from stars.apps.tasks.notifications import (
-    send_welcome_email, send_overdue_notifications,
-    send_post_submission_survey, send_renewal_reminder)
+from stars.apps.tasks.notifications import (send_welcome_email,
+                                            send_post_submission_survey)
 
 
 @shared_task(name='tasks.send_notifications')
@@ -14,16 +13,6 @@ def send_notifications():
     print "Sending Welcome Emails"
     send_welcome_email()
 
-    # Payment Reminders
-    # print "Sending overdue notifications"
-    # send_overdue_notifications()
-    # @todo - this is based on submissionset.date_registered so I think this
-    # doesn't work anymore ... disabled for now
-
     # After Submission
     print "Sending Post Submission Survey"
     send_post_submission_survey()
-
-    # Renewal reminders
-    print "Sending Renewal Reminders"
-    send_renewal_reminder()
