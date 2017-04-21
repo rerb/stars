ea702a1047bb12f04e2e15e4d24b08b32b36faec
Robert Erb <bob.erb@gmail.com>
2017-04-21 00:55:23 -0400

    Patch quick_tools/match_institutions_to_orgs.py

--- stars/quick_tools/match_institutions_to_orgs.py
+++ stars/quick_tools/match_institutions_to_orgs.py
@@ -5,35 +5,19 @@ from apps.institutions.models import Institution, MemberSuiteInstitution
 
 
 for i in Institution.objects.filter(ms_institution=None):
-    try:
-        org = Organization.objects.get(org_name=i.name)
-    except Exception as exc:
-        if i.name in org_ids.keys():
-            try:
-                org = Organization.objects.get(salesforce_id=org_ids[i.name])
-            except Exception as exc2:
-                Organization.DoesNotExist
-                print i.name, exc2
-        else:
-            print i.name, exc
-            continue
-
-    assert(org.salesforce_id != None)
-
-    i.salesforce_id = org.salesforce_id
 
     try:
         msi = MemberSuiteInstitution.objects.get(org_name=i.name)
     except MemberSuiteInstitution.DoesNotExist as exc:
-        print exc
-        pass
+        print i, exc
+        continue
     except MemberSuiteInstitution.MultipleObjectsReturned as exc:
-        print exc
-        pass
-    else:
-        i.ms_institution = msi
+        msi = MemberSuiteInstitution.objects.filter(
+            org_name=i.name).order_by("account_num")[0]
+
+    i.ms_institution = msi
 
     try:
         i.save()
     except Exception as exc:
-        print "COULDN'T SAVE INSTITUTION: ", exc
+        print "COULDN'T SAVE INSTITUTION: ", i, exc
