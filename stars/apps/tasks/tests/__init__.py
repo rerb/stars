72fca8ac77f275a1fc0047802bc454b97f55e6dc
Robert Erb <bob.erb@gmail.com>
2017-12-04 19:06:03 -0500

    Remove subscription monitoring tests
    Cause we don't do subscription monitoring in STARS no more.  That's
    MemberSuite work.

--- stars/apps/tasks/tests/__init__.py
+++ stars/apps/tasks/tests/__init__.py
@@ -1,9 +1,7 @@
 from notify_tests import NotificationTest
 from test_add_months import AddMonthsTest
-from subscription_monitor_test import SubscriptionMonitorTest
 
 __test__ = {
     'notify': NotificationTest,
     'add_months': AddMonthsTest,
-    'subscription_monitor_test': SubscriptionMonitorTest,
 }
