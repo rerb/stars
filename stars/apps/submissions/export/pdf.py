f7d04ba1bf2e21bfa0dc784fe02e43f55df9de20
Robert Erb <bob.erb@gmail.com>
2017-12-29 14:31:50 -0500

    Determine report type and pass in template context

--- stars/apps/submissions/export/pdf.py
+++ stars/apps/submissions/export/pdf.py
@@ -36,30 +36,46 @@ def link_path_callback(path):
     return os.path.join(settings.MEDIA_ROOT, path)
 
 
-def build_report_pdf(submission_set, template=None):
-    """
-    Build a PDF export of a specific submission.
+def build_report_pdf(submissionset, template=None):
+    """Build a PDF export of a specific submission.
 
     If the institution has a valid full access subscription,
     scores are included.
 
-    """
-    context = {'ss': submission_set,
-               'show_scores': submission_set.institution.is_participant,
-               'preview': False,
-               'media_root': settings.MEDIA_ROOT,
-               'project_path': settings.PROJECT_PATH,
-               'rating': submission_set.get_STARS_rating(),
-               'institution': submission_set.institution,
-               'host': "stars.aashe.org",
-               'about_text': Category.objects.get(slug='about').content,
-               'pdf': True}
+    We build reports for three types of submissionsets.  1. Current
+    submissions, which produce Preview reports, 2. Rated submissions,
+    and 3. submissionsets that represent snapshots, cut to share data.
 
-    if submission_set.status != 'r':
-        context['preview'] = True
+    A submissionset is sorted into one of these three types like this:
+
+        Is submissionset status "f"?  If so, it's a snapshot.
+
+        Is submissionset status "r"?  Then it's #2, a rated submission.
+
+        Else, that's a current submission (Preview).
+
+    """
+    if submissionset.status == 'f':
+        report_type = "snapshot"
+    elif submissionset.status == 'r':
+        report_type = "rated"
+    else:
+        report_type = "preview"
+
+    context = {"ss": submissionset,
+               "show_scores": submissionset.institution.is_participant,
+               "report_type": report_type,
+               "preview": report_type == "preview",
+               "media_root": settings.MEDIA_ROOT,
+               "project_path": settings.PROJECT_PATH,
+               "rating": submissionset.get_STARS_rating(),
+               "institution": submissionset.institution,
+               "host": "stars.aashe.org",
+               "about_text": Category.objects.get(slug="about").content,
+               "pdf": True}
 
     if not template:
-        template = 'institutions/pdf/report.html'
+        template = "institutions/pdf/report.html"
 
     return render_to_pdf(template, context)
 
@@ -68,7 +84,7 @@ def build_certificate_pdf(ss):
     """
         Build a PDF certificate for Institution Presidents
     """
-    context = {'ss': ss,
-               'project_path': settings.PROJECT_PATH}
+    context = {"ss": ss,
+               "project_path": settings.PROJECT_PATH}
 
-    return render_to_pdf('institutions/pdf/certificate.html', context)
+    return render_to_pdf("institutions/pdf/certificate.html", context)
