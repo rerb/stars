60623c8f95872fed346ffa3ac09e65da4d454e7d
Tylor Dodge <tylor@tylorbook.local>
2018-04-03 14:57:04 -0400

    Update form invalid text

--- stars/static/js/unsaved_data.js
+++ stars/static/js/unsaved_data.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 
     e.preventDefault();
     var thisHref = $(this).attr('href');
+
     if($('#data-changed').hasClass("form-has-changed")){
 			$("#id_submission_status").val('p');
       $('#unsaved-data').modal();
@@ -16,25 +17,31 @@ $(document).ready(function(){
 				$('#modal-p').hide();
 				$('.spinner').slideDown();
         var frm = $('.submit_form');
-        //fill in this ajax
+
 				$.ajax({
             type: frm.attr('method'),
             url: frm.attr('action'),
             data: frm.serialize(),
             success: function (data) {
 
-							  console.log(data);
-
 								if(data.indexOf("errorlist") >= 0){
 									$('.spinner').hide();
-									$('#modal-p').html("Hmmm, there's an issue...");
+									$('#myModalLabel').html("Errors found");
+									$('#modal-p').html("It appears that there were some issues"
+									  + " with the new data. If you would like to make sure that"
+										+ " your changes are saved, you can validate"
+										+ " them by using the save as 'In progress'"
+										+ " button at the bottom of the form. Otherwise, continue"
+										+ " without saving.");
 									$('#modal-p').show();
+									$('.modal-footer').show();
+									$('.modal-stash-changes').hide();
 								}
 								else{
 									$('.spinner').hide();
 									$('.checkmark').show();
 									window.location.href = thisHref;
-							}		
+							}
 
             },
             error: function(data) {
