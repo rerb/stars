e07d77bf90ed07091463ee23330c3c15b54e9c64
Tylor Dodge <tylor.dodge@aashe.org>
2018-04-10 09:48:43 -0400

    Exclude dropdowns from unsaved-data trigger

--- stars/static/js/unsaved_data.js
+++ stars/static/js/unsaved_data.js
@@ -1,60 +1,64 @@
 $(document).ready(function(){
 
 	$('a').click(function(e){
+		if($(this).hasClass('dropdown-toggle') == false && $(this).hasClass('credit-info-popout') == false){
 
-    e.preventDefault();
-    var thisHref = $(this).attr('href');
+			e.preventDefault();
+			var thisHref = $(this).attr('href');
 
-    if($('#data-changed').hasClass("form-has-changed")){
-			$("#id_submission_status").val('p');
-      $('#unsaved-data').modal();
-      $('.modal-close-button').click(function(){
-        window.location.href = thisHref;
-      });
-			$('.modal-stash-changes').click(function(){
-				$('#myModalLabel').html("One moment...");
-				$('.modal-footer').hide();
-				$('#modal-p').hide();
-				$('.spinner').slideDown();
-        var frm = $('.submit_form');
+			if($('#data-changed').hasClass("form-has-changed")){
 
-				$.ajax({
-            type: frm.attr('method'),
-            url: frm.attr('action'),
-            data: frm.serialize(),
-            success: function (data) {
+				$("#id_submission_status").val('p');
+				$('#unsaved-data').modal();
+				$('.modal-close-button').click(function(){
+					window.location.href = thisHref;
+				});
+				$('.modal-stash-changes').click(function(){
+					$('#myModalLabel').html("One moment...");
+					$('.modal-footer').hide();
+					$('#modal-p').hide();
+					$('.spinner').slideDown();
+					var frm = $('.submit_form');
 
-								if(data.indexOf("errorlist") >= 0){
-									$('.spinner').hide();
-									$('#myModalLabel').html("Errors found");
-									$('#modal-p').html("It appears that there were some issues"
-									  + " with the new data. If you would like to make sure that"
-										+ " your changes are saved, you can validate"
-										+ " them by using the save as 'In progress'"
-										+ " button at the bottom of the form. Otherwise, continue"
-										+ " without saving.");
-									$('#modal-p').show();
-									$('.modal-footer').show();
-									$('.modal-stash-changes').hide();
+					$.ajax({
+							type: frm.attr('method'),
+							url: frm.attr('action'),
+							data: frm.serialize(),
+							success: function (data) {
+
+									if(data.indexOf("errorlist") >= 0){
+										$('.spinner').hide();
+										$('#myModalLabel').html("Errors found");
+										$('#modal-p').html("It appears that there were some issues"
+											+ " with the new data. If you would like to make sure that"
+											+ " your changes are saved, you can validate"
+											+ " them by using the save as 'In progress'"
+											+ " button at the bottom of the form. Otherwise, continue"
+											+ " without saving.");
+										$('#modal-p').show();
+										$('.modal-footer').show();
+										$('.modal-stash-changes').hide();
+									}
+									else{
+										$('.spinner').hide();
+										$('.checkmark').show();
+										window.location.href = thisHref;
 								}
-								else{
-									$('.spinner').hide();
-									$('.checkmark').show();
-									window.location.href = thisHref;
+
+							},
+							error: function(data) {
+									$("#modal-p").html("Something went wrong!");
 							}
+					});
 
-            },
-            error: function(data) {
-                $("#modal-p").html("Something went wrong!");
-            }
-        });
+				});
+			}
+			else{
+				window.location.href = thisHref;
+			}
 
-			});
-    }
-    else{
-      window.location.href = thisHref;
-    }
+			}
+		});
 
-	});
 
 });
