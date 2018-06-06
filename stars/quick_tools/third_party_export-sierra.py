fa7e3c1a7bac5fa0ae909295bcaa86507e9cb770
Robert Erb <bob.erb@gmail.com>
2018-06-06 16:13:17 -0400

    Upgrade django-membersuite-auth

--- stars/quick_tools/third_party_export-sierra.py
+++ stars/quick_tools/third_party_export-sierra.py
@@ -8,77 +8,165 @@ import os
 import string
 
 from stars.apps.credits.models import CreditSet
+from stars.apps.submissions.models import (CreditUserSubmission,
+                                           SubcategorySubmission,
+                                           SubmissionSet,
+                                           SUBMISSION_STATUSES)
 from stars.apps.third_parties.models import ThirdParty
 from stars.apps.third_parties.utils import export_credit_csv
 
 
-limit_inst_ids = [
-    # if this has ids, then only these institutions will be exported
-]
-
-credit_sets = [CreditSet.objects.get(version=2.0),
-               CreditSet.objects.get(version=2.1)]
+START_DATE = datetime.date(year=2015, month=3, day=2)
+END_DATE = datetime.date(year=2018, month=3, day=30)
 
 
-for cs in credit_sets:
+"""
+Sierra needs
+- summary scores for each category and subcategory
+  (presented as points earned over points possible and
+  including Innovation & Leadership) and
+- all credit-level data from every rated report submitted between
+  March 2, 2015 and March 30, 2018
+  by institutions who have opted in to have their data shared with Sierra.
+- Data from Reporter-level reports should not be included
+  nor should data from reports that are still in review and revision mode.
+"""
 
-    tp = ThirdParty.objects.get(slug="sierra")
-    print "EXPORTING: %s" % tp
 
-    start_date = datetime.date(year=2016, month=3, day=4)
-    end_date = datetime.date(year=2017, month=3, day=13)
+def get_institution_institution_type_from_submission_set(submission_set):
 
-    snapshot_list = tp.get_snapshots().order_by("institution__name")
-    snapshot_list = snapshot_list.filter(creditset=cs)
-    snapshot_list = snapshot_list.filter(date_submitted__gte=start_date)
-    snapshot_list = snapshot_list.filter(date_submitted__lte=end_date)
+    credit_user_submission = CreditUserSubmission.objects.get(
+        credit__identifier="IC-1",
+        subcategory_submission__category_submission__submissionset=submission_set)  # noqa
 
-    if(limit_inst_ids):
-        snapshot_list = snapshot_list.filter(
-            institution__id__in=limit_inst_ids)
+    field = credit_user_submission.get_submission_fields()[0]
 
-    inst_list = []
-    latest_snapshot_list = []  # only use the latest snapshot
-    for ss in snapshot_list:
-        if ss.institution.id not in inst_list:
-            inst_list.append(ss.institution.id)
-            i_ss_list = ss.institution.submissionset_set.filter(
-                status='f').order_by('-date_submitted', '-id').filter(
-                    date_submitted__gte=start_date).filter(
-                        date_submitted__lte=end_date)
-            print "Available snapshots for %s" % ss.institution
-            for __ss in i_ss_list:
-                print "%s, %d" % (__ss.date_submitted, __ss.id)
-            if i_ss_list[0].creditset == cs:
-                latest_snapshot_list.append(i_ss_list[0])
-            else:
-                print "newer snapshot in version: %s" % i_ss_list[0].creditset
-            print "Selected snapshot:%s, %d" % (
-                i_ss_list[0].date_submitted, i_ss_list[0].id)
+    return str(field.value)
 
-    for cat in cs.category_set.all():
-        for sub in cat.subcategory_set.all():
-            for c in sub.credit_set.all():
+
+sierra = ThirdParty.objects.get(slug="sierra")
+
+summaries = {}
+
+
+for creditset in [CreditSet.objects.get(version="2.0"),
+                  CreditSet.objects.get(version="2.1")]:
+
+    reports = SubmissionSet.objects.filter(
+        institution__in=sierra.access_to_institutions.all()).exclude(
+            rating__isnull=True).filter(
+                creditset=creditset).filter(
+                    date_submitted__gte=START_DATE).filter(
+                        date_submitted__lte=END_DATE).order_by(
+                            "institution__name",
+                            "-date_submitted",
+                            "-id").filter(
+                                status=SUBMISSION_STATUSES["RATED"]).exclude(
+                                    institution__name="AASHE Test Campus").exclude(  # noqa
+                                        rating__name="Reporter")
+    institutions = []
+    latest_reports = []  # only use the latest report
+
+    for report in reports:
+        # Just take first report for each institution.
+        if report.institution in institutions:
+            continue  # Not the first report for this institution.
+
+        if creditset.version == "2.0":
+            # if this inst has a 2.1 report, don't include the 2.0 one.
+            if SubmissionSet.objects.filter(
+                institution=report.institution).exclude(
+                    rating__isnull=True).filter(
+                        creditset=CreditSet.objects.get(version="2.1")).filter(  # noqa
+                            date_submitted__gte=START_DATE).filter(
+                                date_submitted__lte=END_DATE).filter(
+                                    status=SUBMISSION_STATUSES["RATED"]).exclude(  # noqa
+                                        institution__name="AASHE Test Campus").exclude(  # noqa
+                                            rating__name="Reporter").count():  # noqa
+                continue
+        latest_reports.append(report)
+        institutions.append(report.institution)
+        summaries[report.institution] = report
+
+    for cat in creditset.category_set.all():
+        for subcategory in cat.subcategory_set.all():
+            for credit in subcategory.credit_set.all():
                 filename = 'sierra/export/%s/%s.csv' % (
-                    cs.version, string.replace("%s" % c, "/", "-"))
+                    creditset.version, string.replace("%s" % credit,
+                                                      "/", "-"))
                 filename = string.replace(filename, ":", "")
                 filename = string.replace(filename, " ", "_")
 
-                export_credit_csv(c,
-                                  ss_qs=latest_snapshot_list,
+                export_credit_csv(credit,
+                                  ss_qs=latest_reports,
                                   outfilename=filename)
 
-    # export pdfs
-    count = 0
-    total = len(latest_snapshot_list)
-    pdf_path = "./sierra/export/pdf"
-    if not os.path.exists(pdf_path):
-        os.makedirs(pdf_path)
-    for ss in latest_snapshot_list:
-        count += 1
-        print ss
-        print "%d of %d" % (count, total)
-        outfile = os.path.join(pdf_path, ss.get_pdf_filename())
-        pdf = ss.get_pdf(refresh=False,
-                         template='institutions/pdf/third_party_report.html')
-        os.rename(pdf, outfile)
+    # export subcategory score sheets
+    filename = "%s_subcategory_scores.tsv" % creditset.version
+    outfile = open(filename, 'w+')
+
+    subcategory_headers = [
+        "Institution Name",
+        "Status",
+        "Submit Date",
+        "STARS version",
+        "Overall Score",
+        "Rating",
+        "Institution Type",
+        "Country",
+        "Nth Report (includes expired)",
+        "# Reports Submitted (includes expired)",
+    ]
+
+    # Add the subcategories
+    subcategories = []  # stored for later reference
+    for category in creditset.category_set.all():
+        for subcategory in category.subcategory_set.all():
+            subcategories.append(subcategory)
+            subcategory_headers.append(
+                "%s points claimed" % subcategory.title)
+            subcategory_headers.append(
+                "%s points available" % subcategory.title)
+
+    print >>outfile, "\t".join(subcategory_headers)
+
+    for report in latest_reports:
+        row = []
+        row.append(report.institution.name)
+        row.append(report.status)
+        row.append(unicode(report.date_submitted))
+        row.append(report.creditset.version)
+        row.append("%f.2" % report.score if report.score else "0.00")
+        row.append(report.rating.name)
+        row.append(
+            get_institution_institution_type_from_submission_set(report))
+        row.append(report.institution.country)
+
+        # calculated total reports submitted
+        all_reports = report.institution.submissionset_set.filter(
+            status=SUBMISSION_STATUSES["RATED"]).exclude(
+                rating__name="Reporter").order_by(
+                    'date_submitted').values_list(
+                        'id', 'date_submitted')
+        index = [x[0] for x in all_reports].index(report.id)
+
+        row.append(unicode(index + 1))
+        row.append(unicode(len(all_reports)))
+
+        for sub in subcategories:
+            subcatsub = SubcategorySubmission.objects.filter(
+                subcategory=sub).get(category_submission__submissionset=report)
+            row.append(unicode(subcatsub.get_claimed_points()))
+            row.append(unicode(subcatsub.get_adjusted_available_points()))
+
+        print >>outfile, u"\t".join(row)
+
+    outfile.close()
+
+print '\t'.join(("Institution", "Date Submitted", "Rating",
+                 "Version", "Liaison Email"))
+
+for institution, report in summaries.items():
+    print '\t'.join([institution.name, str(report.date_submitted),
+                     report.rating.name, report.creditset.version,
+                     institution.contact_email])
