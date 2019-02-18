b06cac38e3012dda3c32cb183cb5e4ef99541edc
Tylor Dodge <tylor.dodge@aashe.org>
2019-02-18 14:53:22 -0500

    Remove lonely print.

--- stars/apps/bt_etl/management/commands/ns_fault_check.py
+++ stars/apps/bt_etl/management/commands/ns_fault_check.py
@@ -3,10 +3,11 @@ from django.conf import settings
 
 from stars.apps.submissions.models import NumericSubmission
 
+
 class Command(BaseCommand):
     help = """
         Runs report on fault state of Numeric Submission.
-        
+
         Ideally, NumericSubmissions should have the same value for
         metric and imperial.
     """
@@ -17,19 +18,18 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         qs = NumericSubmission.objects.filter(
             documentation_field__units__isnull=True)
-        
-        print
+
         print("Total NumericSubmissions with no units: %d" % qs.count())
-        
+
         # qs = qs.filter(
         #     documentation_field__title__contains="percentage")
         # print("Of those, %d, say 'percentage' in the title" % qs.count())
-            
+
         print("""
         Each of these should have the same imperial and metric values,
         or the metric value should be None.
         """)
-        
+
         fault_count = 0
         faulty_reports = []
         qs = qs.filter(value__isnull=False)
@@ -40,10 +40,10 @@ class Command(BaseCommand):
                 ss = ns.credit_submission.creditusersubmission.subcategory_submission.category_submission.submissionset
                 if ss not in faulty_reports:
                     faulty_reports.append(ss)
-        
+
         print("\n%d (%.2f%%) fields fail that" % (
-            fault_count, ( 100.0 * fault_count / qs.count())))
-            
+            fault_count, (100.0 * fault_count / qs.count())))
+
         print("\nin %d reports\n\n" % len(faulty_reports))
         for ss in faulty_reports:
             print(ss)
