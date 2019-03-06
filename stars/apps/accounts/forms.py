29f621d2af36eba42f11857a12706e6e1b30d628
Tylor Dodge <tylor.dodge@aashe.org>
2019-03-06 15:23:10 -0500

    Remove Terms of Service.

--- stars/apps/accounts/forms.py
+++ stars/apps/accounts/forms.py
@@ -4,17 +4,8 @@ from django.contrib.auth.forms import AuthenticationForm
 
 from stars.apps.institutions.models import StarsAccount
 
+
 class LoginForm(AuthenticationForm):
     """ Extends the AuthenticationForm to use longer usernames """
-    
-    username = forms.CharField(label="Email", max_length=100)
 
-class TOSForm(ModelForm):
-    """
-        Form that allows users to check the Terms of Service
-    """
-    terms_of_service = forms.BooleanField(label='I agree to the terms of service', required=True)
-    
-    class Meta:
-        model = StarsAccount
-        fields = ['terms_of_service',]
+    username = forms.CharField(label="Email", max_length=100)
