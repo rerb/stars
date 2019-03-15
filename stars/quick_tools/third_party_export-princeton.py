3130318f65d78e1e756ce621477f7e9a022895e7
Robert Erb <bob.erb@aashe.org>
2019-03-15 14:41:24 -0400

    Fix Institution.get_liaison_name() to handle Nones

--- stars/quick_tools/third_party_export-princeton.py
+++ stars/quick_tools/third_party_export-princeton.py
@@ -17,7 +17,7 @@ for cs in [CreditSet.objects.get(version=2.0),
            CreditSet.objects.get(version=2.1)]:
 
     start_date = datetime.date(year=2016, month=3, day=2)
-    end_date = datetime.date(year=2019, month=3, day=8)
+    end_date = datetime.date(year=2019, month=3, day=11)
 
     reports = SubmissionSet.objects.filter(
         institution__in=tp.access_to_institutions.all()).exclude(
