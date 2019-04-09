1f983131dfae017e50c6192665173a69ff315fe7
Robert Erb <rerb@mbp.local>
2019-04-09 06:03:24 -0400

    Update kill_me_now.py

--- stars/quick_tools/kill_me_now.py
+++ stars/quick_tools/kill_me_now.py
@@ -1,4 +1,5 @@
 import datetime
+import pickle
 
 from stars.apps.institutions.management.commands import restore_institution
 from stars.apps.submissions.models import CreditUserSubmission
@@ -6,23 +7,49 @@ from stars.apps.submissions.models import CreditUserSubmission
 
 def check_match(left, right):
 
-    left_submissionset, right_submissionset = (
-        left.get_submissionset(), right.get_submissionset())
-
-    if left_submissionset.pk == right_submissionset.pk:
-        return (True, None)
+    if left["cus"].credit.title != right["cus"].credit.title:
+        return (False, "credit.title values differ (left: {left_credit_title}, "
+                "right: {right_credit_title}".format(
+                    left_credit_title=left["cus"].credit.title,
+                    right_credit_title=right["cus"].credit.title))
+    elif (left["cus"].subcategory_submission.subcategory.title !=
+          right["cus"].subcategory_submission.subcategory.title):
+        return (False, "subcategory_submission.subcategory.title values differ "
+                "(left: {left_credit_title}, right: {right_credit_title}".format(
+                    left_credit_title=left["cus"].subcategory_submission.subcategory.title,
+                    right_credit_title=right["cus"].subcategory_submission.subcategory.title))
+    elif (left["cus"].subcategory_submission.category_submission.category.title !=
+          right["cus"].subcategory_submission.category_submission.category.title):
+        return (False, "subcategory_submission.category_submission.category.title "
+                "values differ (left: {left_category_title}, "
+                "right: {right_category_title}".format(
+                    left_category_title=left["cus"].subcategory_submission.category_submission.category.title,
+                    right_category_title=right["cus"].subcategory_submission.category_submission.category.title))
     else:
-        return (False, "get_submissionset().pk values differ")
+        if left["submissionset"].pk != right["submissionset"].pk:
+            return (False, "submissionset.pk values differ, left: {left_pk}, "
+                    "right: {right_pk}".format(left_pk=left["submissionset"].pk,
+                                               right_pk=right["submissionset"].pk))
+        elif (left["submissionset"].institution.name !=
+              right["submissionset"].institution.name):
+            return (False, "submissionset.institution.name values differ, "
+                    "left: {left_name}, right: {right_name}".format(
+                        left_name=left["submissionset"].institution.name,
+                        right_name=right["submissionset"].institution.name))
+        else:
+            return (True, None)
 
 
 matched = []
 unmatched = []
-updated = live_is_more_recent = 0
+updated = []
+live_is_more_recent = []
+exceptions = []
 
 
 def update_all_credit_user_submissions():
 
-    global matched, unmatched, updated, live_is_more_recent
+    global matched, unmatched, updated, live_is_more_recent, exceptions
 
     for cus in CreditUserSubmission.objects.using("stars-backup").filter(
             last_updated__gt=datetime.date(2019, 04, 01)):
@@ -31,36 +58,126 @@ def update_all_credit_user_submissions():
             live_match = CreditUserSubmission.objects.using("default").get(
                 pk=cus.pk)
         except CreditUserSubmission.DoesNotExist:
-            unmatched.append(cus)
+            unmatched.append({"cus": cus,
+                              "submissionset": cus.get_submissionset(),
+                              "error_message": "no match on CreditUserSubmission.pk"})
         else:
-            matches, error_message = check_match(cus, live_match)
+            cus_submissionset = cus.get_submissionset()
+            match_submissionset = live_match.get_submissionset()
+            matches, error_message = check_match(
+                {"cus": cus, "submissionset": cus.get_submissionset()},
+                {"cus": live_match, "submissionset": match_submissionset})
             if matches:
-                matched.append(cus)
+                matched.append({"cus": cus,
+                                "submissionset": cus_submissionset})
+                if live_match.last_updated < cus.last_updated:
+                    try:
+                        restore_institution.restore_credit_user_submission(
+                            credit_user_submission=cus,
+                            source_db="stars-backup",
+                            target_db="default")
+                    except Exception as exc:
+                        print live_match.pk, " not updated:", exc
+                        exceptions.append({"cus": live_match,
+                                           "submissionset": match_submissionset,
+                                           "exception": exc})
+                    else:
+                        updated.append({"cus": live_match,
+                                        "submissionset": match_submissionset})
+                else:
+                    live_is_more_recent.append({"cus": live_match,
+                                                "submissionset": match_submissionset})
             else:
                 print cus.pk, "check_match error:", error_message
-                unmatched.append(cus)
+                unmatched.append({"cus": cus,
+                                  "submissionset": cus_submissionset,
+                                  "error_message": error_message})
 
-        if live_match.last_updated < cus.last_updated:
-            try:
-                restore_institution.restore_credit_user_submission(
-                    credit_user_submission=cus,
-                    source_db="stars-backup",
-                    target_db="default")
-            except Exception as exc:
-                print live_match.pk, " not updated:", exc
-            else:
-                updated += 1
-
-        else:
-            live_is_more_recent += 1
+def recalculate_submissions():
+    for submissionset in set([cus["submissionset"] for cus in updated]):
+        print "recalculating", submissionset,
+        print " was", submissionset.get_STARS_score(),
+        print " now", submissionset.get_STARS_score(recalculate=True)
 
 
 def report():
-    print(len(matched), "matched")
-    print(len(unmatched), "unmatched")
-    print("updated: " + str(updated))
-    print("live_is_more_recent: " + str(live_is_more_recent))
 
+    # reports_url = "http://localhost:8000"
+    reports_url = "https://reports.aashe.org"
+
+    def dressed_manage_url(submissionset):
+        return reports_url + submissionset.get_manage_url()
+
+    def dressed_scorecard_url(credit_user_submission):
+        return reports_url + credit_user_submission.get_scorecard_url()
+
+    def dressed_submit_url(credit_user_submission):
+        return reports_url + credit_user_submission.get_submit_url()
+
+    print("matched: ", len(matched))
+    print("unmatched: ", len(unmatched))
+    print("updated:", len(updated))
+    print("live_is_more_recent:", len(live_is_more_recent))
+    print("exceptions:", len(exceptions))
+    print
+    print
+    print("updated submissionsets:")
+    print
+    for submissionset in sorted(
+            set([cus["submissionset"] for cus in updated]),
+            key=lambda k: k.institution.name):
+        print "* updated:", submissionset, '(' + str(submissionset.pk) + ')'
+        print "          ", dressed_manage_url(submissionset=submissionset)
+        for cus in [
+                cus for cus in updated if cus["submissionset"] == submissionset]:
+            print "           credit:", cus["cus"].credit
+            print "                  ", dressed_scorecard_url(cus["cus"])
+            print "                  ", dressed_submit_url(cus["cus"])
+        print
+
+    print("live is newer submissionsets:")
+    print
+    for submissionset in sorted(
+            set([cus["cus"].get_submissionset() for cus in live_is_more_recent]),
+            key=lambda k: k.institution.name):
+        print "* live is newer:", submissionset, '(' + str(submissionset.pk) + ')'
+        print "                ", dressed_manage_url(submissionset=submissionset)
+        for cus in [
+                cus for cus in live_is_more_recent if cus["submissionset"] == submissionset]:
+            print "           credit:", cus["cus"].credit
+            print "                  ", dressed_scorecard_url(cus["cus"])
+            print "                  ", dressed_submit_url(cus["cus"])
+        print
+
+    print("unmatched submissionsets:")
+    print
+    for submissionset in sorted(
+            set([cus["cus"].get_submissionset() for cus in unmatched]),
+            key=lambda k: k.institution.name):
+        print "* unmatched:", submissionset, '(' + str(submissionset.pk) + ')'
+        print "            ", dressed_manage_url(submissionset=submissionset)
+        for cus in [
+                cus for cus in unmatched if cus["submissionset"] == submissionset]:
+            print "           credit:", cus["cus"].credit
+            print "                  ", cus["error_message"]
+            print "                  ", dressed_scorecard_url(cus["cus"])
+            print "                  ", dressed_submit_url(cus["cus"])
+        print
+
+    print("exceptions:")
+    print
+    for submissionset in sorted(
+            set([cus["cus"].get_submissionset() for cus in exceptions]),
+            key=lambda k: k.institution.name):
+        print "* exception:", submissionset, '(' + str(submissionset.pk) + ')'
+        print "            ", dressed_manage_url(submissionset=submissionset)
+        for cus in [
+                cus for cus in exceptions if cus["submissionset"] == submissionset]:
+            print "           credit:", cus["cus"].credit
+            print "                  ", dressed_scorecard_url(cus["cus"])
+            print "                   ", dressed_submit_url(cus["cus"])
+            print "                  ", cus["exception"]
+        print
 
 def handle_unmatched():
 
